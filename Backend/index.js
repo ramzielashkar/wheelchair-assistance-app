5c243fc06e22938c6201610c52b76ab3237d3e76
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-26 15:37:38 +0300

    Backend: Including client routes

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,9 @@ const cors = require ('cors');
 
 app.use(cors());
 
+const clientRoutes = require('./routes/client.routes');
+app.use('/client', clientRoutes)
+
 app.listen(process.env.PORT, (err)=>{
     if(err) throw err;
     console.log(`server running on port ${process.env.PORT}`);
