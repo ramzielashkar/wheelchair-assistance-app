93d51773c1474c37118451ef3d086c2bdaa1cca1
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-26 18:25:28 +0300

    Backend: Including get active service providers route

--- Backend/routes/admin.routes.js
+++ Backend/routes/admin.routes.js
@@ -1,9 +1,10 @@
 const {Router} = require('express');
-const { addUser, login } = require('../controllers/admin.controller');
+const { addUser, login, getServiceProviders } = require('../controllers/admin.controller');
 const adminMiddleware = require('../middlewares/admin.middleware');
 const router = Router();
 
 router.post('/login', login);
 router.post('/addUser', adminMiddleware, addUser);
+router.get('/services', adminMiddleware, getServiceProviders);
 
 module.exports = router
