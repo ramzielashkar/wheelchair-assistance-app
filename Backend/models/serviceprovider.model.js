f3a0a94920b32f7f67304280174745868289e68d
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-26 15:06:16 +0300

    Backend: Creating service provider model

---
+++ Backend/models/serviceprovider.model.js
@@ -0,0 +1,54 @@
+const { default: mongoose } = require('mongoose');
+const {User, userSchema} = require('./user.model');
+const Client = require('./client.model');
+const { schema } = require('./user.model');
+
+
+const Seller = User.discriminator('Seller', new mongoose.Schema({
+    profile_picture: {
+        type: String,
+    },
+    location:{
+        type: String,
+    },
+    followers:[
+        {
+            follower_id : {
+                type: mongoose.Schema.Types.ObjectId,
+                ref:'Client'
+            }
+        }
+    ],
+    active:{
+        type: Boolean,
+    },
+    description:{
+        type:String,
+    },
+    working_hours:{
+        type:String,
+    },
+    phone_number:{
+        type : Number,
+    },
+    pictures:[
+        {
+            picture:{
+                type:String,
+            }
+        }
+    ],
+    type:{
+        type:String,
+    },
+    geo_location:{
+        type : {type : String},
+        coordinates : [],
+    }
+
+}),
+);
+
+
+
+module.exports= Seller
