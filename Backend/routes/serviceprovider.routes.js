5794f25f02a585fa39f4ef8ad3aa213294ae677a
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-26 20:44:55 +0300

    Backend: Including service provider add picture route

--- Backend/routes/serviceprovider.routes.js
+++ Backend/routes/serviceprovider.routes.js
@@ -1,11 +1,12 @@
 const {Router} = require('express');
-const { login, updateprofilepic, editProfile } = require('../controllers/serviceprovider.controller');
+const { login, updateprofilepic, editProfile, addPicture } = require('../controllers/serviceprovider.controller');
 const serviceProviderMiddleware = require('../middlewares/serviceprovider.middleware');
 const router = Router();
 
 router.post('/login', login);
 router.put('/profilepic', serviceProviderMiddleware, updateprofilepic);
 router.put('/editprofile', serviceProviderMiddleware, editProfile);
+router.post('/picture', serviceProviderMiddleware, addPicture);
 
 
 
