25ae5e60f12f1191c3e160410b0b8ee9f2efd557
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-26 20:48:26 +0300

    Backend: Including service provider get pictures route

--- Backend/routes/serviceprovider.routes.js
+++ Backend/routes/serviceprovider.routes.js
@@ -1,5 +1,5 @@
 const {Router} = require('express');
-const { login, updateprofilepic, editProfile, addPicture } = require('../controllers/serviceprovider.controller');
+const { login, updateprofilepic, editProfile, addPicture, getPictures } = require('../controllers/serviceprovider.controller');
 const serviceProviderMiddleware = require('../middlewares/serviceprovider.middleware');
 const router = Router();
 
@@ -7,6 +7,7 @@ router.post('/login', login);
 router.put('/profilepic', serviceProviderMiddleware, updateprofilepic);
 router.put('/editprofile', serviceProviderMiddleware, editProfile);
 router.post('/picture', serviceProviderMiddleware, addPicture);
+router.get('/picture', serviceProviderMiddleware, getPictures);
 
 
 
