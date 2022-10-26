6c940d193494ad84a1556ffefa4a4e9b442bab97
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-26 19:02:45 +0300

    Backend: Including ban/unban service providers route

--- Backend/routes/admin.routes.js
+++ Backend/routes/admin.routes.js
@@ -1,5 +1,5 @@
 const {Router} = require('express');
-const { addUser, login, getServiceProviders, getBannedServiceProviders } = require('../controllers/admin.controller');
+const { addUser, login, getServiceProviders, getBannedServiceProviders, toggleActiveClient } = require('../controllers/admin.controller');
 const adminMiddleware = require('../middlewares/admin.middleware');
 const router = Router();
 
@@ -7,6 +7,7 @@ router.post('/login', login);
 router.post('/addUser', adminMiddleware, addUser);
 router.get('/services', adminMiddleware, getServiceProviders);
 router.get('/services/banned', adminMiddleware, getBannedServiceProviders);
+router.put('/banClient/:id', adminMiddleware, toggleActiveClient);
 
 
 module.exports = router
