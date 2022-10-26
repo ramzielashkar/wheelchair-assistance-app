c68ac4a80186826cc09e6362288ade2a310dd536
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-26 15:00:06 +0300

    Backend: Setting up my server

---
+++ Backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+//require('./config/db.config');
+require('dotenv').config();
+const cors = require ('cors');
+
+app.use(cors());
+
+app.listen(process.env.PORT, (err)=>{
+    if(err) throw err;
+    console.log(`server running on port ${process.env.PORT}`);
+})
