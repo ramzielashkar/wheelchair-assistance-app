490dcf0513673696b531488947fbe32d08ba283a
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-26 21:04:34 +0300

    Backend: Including service provider delete picture route

--- Backend/routes/serviceprovider.routes.js
+++ Backend/routes/serviceprovider.routes.js
@@ -1,5 +1,5 @@
 const {Router} = require('express');
-const { login, updateprofilepic, editProfile, addPicture, getPictures } = require('../controllers/serviceprovider.controller');
+const { login, updateprofilepic, editProfile, addPicture, getPictures, deletePicture } = require('../controllers/serviceprovider.controller');
 const serviceProviderMiddleware = require('../middlewares/serviceprovider.middleware');
 const router = Router();
 
@@ -8,6 +8,7 @@ router.put('/profilepic', serviceProviderMiddleware, updateprofilepic);
 router.put('/editprofile', serviceProviderMiddleware, editProfile);
 router.post('/picture', serviceProviderMiddleware, addPicture);
 router.get('/picture', serviceProviderMiddleware, getPictures);
+router.delete('/picture/:picture_id', serviceProviderMiddleware, deletePicture);
 
 
 
