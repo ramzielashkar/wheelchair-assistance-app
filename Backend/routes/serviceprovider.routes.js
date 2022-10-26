f45c8af4ed88f2fa8e3be1f1e8b807ddecdca8c9
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-26 20:35:27 +0300

    Backend: Including service provider edit profile route

--- Backend/routes/serviceprovider.routes.js
+++ Backend/routes/serviceprovider.routes.js
@@ -1,10 +1,12 @@
 const {Router} = require('express');
-const { login, updateprofilepic } = require('../controllers/serviceprovider.controller');
+const { login, updateprofilepic, editProfile } = require('../controllers/serviceprovider.controller');
 const serviceProviderMiddleware = require('../middlewares/serviceprovider.middleware');
 const router = Router();
 
 router.post('/login', login);
 router.put('/profilepic', serviceProviderMiddleware, updateprofilepic);
+router.put('/editprofile', serviceProviderMiddleware, editProfile);
+
 
 
 module.exports = router;
