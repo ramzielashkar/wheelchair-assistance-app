139cc2ec13cf1c48ba5d57cac377e340696bfa39
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-26 16:20:00 +0300

    Backend: Updating client middleware

--- Backend/middlewares/client.middleware.js
+++ Backend/middlewares/client.middleware.js
@@ -1,13 +1,16 @@
 const jwt = require('jsonwebtoken');
-const Client = require('../models/users.model');
+const Client = require('../models/client.model');
 
 // Authentication middleware
 const clientMiddleware = async (req, res, next) => {
-    const token = req.headers.authorization.split(" ")[1];
+    let token = req.headers.authorization;
+    if(!token) return res.status(401).json({message: "Unauthorized"})
+    token = token.split(" ")[1];
     try{
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
         const user = await Client.findOne({email: decoded.email}).lean();
         if(user.usertype != 'Client') return res.status(401).json({message: "Unauthorized"});
+        req.user = {...user, id: user._id};
         next()
 
     }catch(err){
