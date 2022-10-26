8077f997efabb296dc6bdfd4696602048ffe8da0
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-26 18:26:59 +0300

    Backend: Including get banned service providers route

--- Backend/routes/admin.routes.js
+++ Backend/routes/admin.routes.js
@@ -1,10 +1,12 @@
 const {Router} = require('express');
-const { addUser, login, getServiceProviders } = require('../controllers/admin.controller');
+const { addUser, login, getServiceProviders, getBannedServiceProviders } = require('../controllers/admin.controller');
 const adminMiddleware = require('../middlewares/admin.middleware');
 const router = Router();
 
 router.post('/login', login);
 router.post('/addUser', adminMiddleware, addUser);
 router.get('/services', adminMiddleware, getServiceProviders);
+router.get('/services/banned', adminMiddleware, getBannedServiceProviders);
+
 
 module.exports = router
