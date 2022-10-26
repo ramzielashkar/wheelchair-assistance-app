e42710c97d15f07bc73ea3581b52b8a1cacfcbeb
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-26 15:01:59 +0300

    Backend: Connecting to database

---
+++ Backend/config/db.config.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+require('dotenv').config();
+
+mongoose.connect(process.env.DATABASE_URL)
+.then(()=>{
+    console.log("database connected")
+})
+.catch(err=>{
+    console.log("database error: ", err)
+})
+
+
