49bb490a3ea9b0a4165aea1348e8c9265267ed8c
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-26 16:21:01 +0300

    Backend: Creating client update picture route

--- Backend/routes/client.routes.js
+++ Backend/routes/client.routes.js
@@ -1,8 +1,9 @@
 const {Router} = require('express');
-const { register } = require('../controllers/client.controller');
+const { register, updateprofilepic } = require('../controllers/client.controller');
+const clientMiddleware = require('../middlewares/client.middleware');
 const router = Router();
 
 router.post('/signup', register);
-
+router.post('/profilepic', clientMiddleware, updateprofilepic);
 
 module.exports = router
