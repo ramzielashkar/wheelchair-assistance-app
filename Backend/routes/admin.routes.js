19985230fc9efea53e75ef8db7038e6a4e19a623
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-26 19:09:34 +0300

    Backend: Including get number of users route

--- Backend/routes/admin.routes.js
+++ Backend/routes/admin.routes.js
@@ -1,5 +1,5 @@
 const {Router} = require('express');
-const { addUser, login, getServiceProviders, getBannedServiceProviders, toggleActiveClient } = require('../controllers/admin.controller');
+const { addUser, login, getServiceProviders, getBannedServiceProviders, toggleActiveClient, getUsersCount } = require('../controllers/admin.controller');
 const adminMiddleware = require('../middlewares/admin.middleware');
 const router = Router();
 
@@ -8,6 +8,7 @@ router.post('/addUser', adminMiddleware, addUser);
 router.get('/services', adminMiddleware, getServiceProviders);
 router.get('/services/banned', adminMiddleware, getBannedServiceProviders);
 router.put('/banClient/:id', adminMiddleware, toggleActiveClient);
+router.get('/count', adminMiddleware, getUsersCount);
 
 
 module.exports = router
