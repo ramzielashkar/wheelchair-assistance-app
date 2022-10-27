bbbcb7d5cfce6ae24db81fc0f1e30659b9be3d5b
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-27 20:03:56 +0300

    Backend: Including service provider get followers route

--- Backend/routes/serviceprovider.routes.js
+++ Backend/routes/serviceprovider.routes.js
@@ -1,5 +1,5 @@
 const {Router} = require('express');
-const { updateprofilepic, editProfile, addPicture, getPictures, deletePicture } = require('../controllers/serviceprovider.controller');
+const { updateprofilepic, editProfile, addPicture, getPictures, deletePicture, getFollowers } = require('../controllers/serviceprovider.controller');
 const serviceProviderMiddleware = require('../middlewares/serviceprovider.middleware');
 const router = Router();
 
@@ -8,6 +8,7 @@ router.put('/editprofile', serviceProviderMiddleware, editProfile);
 router.post('/picture', serviceProviderMiddleware, addPicture);
 router.get('/picture', serviceProviderMiddleware, getPictures);
 router.delete('/picture/:picture_id', serviceProviderMiddleware, deletePicture);
+router.get('/followers', serviceProviderMiddleware, getFollowers);
 
 
 
