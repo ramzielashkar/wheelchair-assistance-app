302a2b097e626c0cab4c25ec35bbee2defe28f20
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-27 12:44:54 +0300

    Backend: Including get service providers route

--- Backend/routes/client.routes.js
+++ Backend/routes/client.routes.js
@@ -1,10 +1,11 @@
 const {Router} = require('express');
-const { register, updateprofilepic, editProfile } = require('../controllers/client.controller');
+const { register, updateprofilepic, editProfile, getServiceProviders } = require('../controllers/client.controller');
 const clientMiddleware = require('../middlewares/client.middleware');
 const router = Router();
 
 router.post('/signup', register);
 router.put('/profilepic', clientMiddleware, updateprofilepic);
 router.put('/editprofile', clientMiddleware, editProfile);
+router.get('/services', clientMiddleware, getServiceProviders);
 
 module.exports = router
