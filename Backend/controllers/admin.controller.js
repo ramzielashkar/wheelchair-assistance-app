71b1f08ffee5ec0fe7ef3f093c1c64cd08b04856
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-27 19:48:45 +0300

    Backend: Updating admin and service provider login function

--- Backend/controllers/admin.controller.js
+++ Backend/controllers/admin.controller.js
@@ -8,24 +8,6 @@ const { now } = require('mongoose');
 const emailValidator = require('email-validator');
 const passwordSchema = require('../validators/password.validator');
 
-//function to login
-const login = async (req, res)=>{
-    const {email, password} = req.body;
-    
-    const user = await Admin.findOne({email}).select("+password");
-
-    if(!user) return res.status(404).json({message: "Invalid Credentials"});
-    
-    if(!user.password==password) return res.status(404).json({message: "Invalid Credentials"});
-    const token = jwt.sign({email: user.email, name: user.name}, process.env.JWT_SECRET_KEY, {
-        expiresIn: '1h'
-    });
-    res.status(200).json({
-        'user': user, 
-        'token':token
-    })
-}
-
 // function to add service provider
 const addUser = async (req, res) =>{
     const {name, email, password, type, location, geo_location} = req.body;
@@ -127,7 +109,6 @@ const toggleActiveClient = async (req, res)=>{
 
 module.exports= {
     addUser,
-    login,
     getServiceProviders,
     getBannedServiceProviders,
     toggleActiveSellers,
--- Backend/controllers/serviceprovider.controller.js
+++ Backend/controllers/serviceprovider.controller.js
@@ -8,24 +8,6 @@ const { now } = require('mongoose');
 const emailValidator = require('email-validator');
 const passwordSchema = require('../validators/password.validator');
 
-//function to login
-const login = async (req, res)=>{
-    const {email, password} = req.body;
-    
-    const user = await Seller.findOne({email}).select("+password");
-
-    if(!user) return res.status(404).json({message: "Invalid Credentials"});
-    if(user.active==false) return res.status(404).json({message:"User Banned"})
-    
-    if(!user.password==password) return res.status(404).json({message: "Invalid Credentials"});
-    const token = jwt.sign({email: user.email, name: user.name}, process.env.JWT_SECRET_KEY, {
-        expiresIn: '1h'
-    });
-    res.status(200).json({
-        'user': user, 
-        'token':token
-    })
-}
 
 //function to update profile picture
 const updateprofilepic = async (req, res)=>{
@@ -144,7 +126,6 @@ const deletePicture = async (req, res)=>{
 }
 
 module.exports = {
-    login,
     updateprofilepic,
     editProfile,
     addPicture,
