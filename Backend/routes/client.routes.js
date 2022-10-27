243be47942fdec46bed223606f033b17be51b61a
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-27 13:16:18 +0300

    Backend: Including get service provider by Id route

--- Backend/routes/client.routes.js
+++ Backend/routes/client.routes.js
@@ -1,11 +1,13 @@
 const {Router} = require('express');
-const { register, updateprofilepic, editProfile, getServiceProviders } = require('../controllers/client.controller');
+const { register, updateprofilepic, editProfile, getServiceProviders, getServiceProvider } = require('../controllers/client.controller');
 const clientMiddleware = require('../middlewares/client.middleware');
 const router = Router();
 
 router.post('/signup', register);
 router.put('/profilepic', clientMiddleware, updateprofilepic);
 router.put('/editprofile', clientMiddleware, editProfile);
-router.get('/services', clientMiddleware, getServiceProviders);
+router.get('/services/:type', clientMiddleware, getServiceProviders);
+router.get('/service/:_id', clientMiddleware, getServiceProvider);
+
 
 module.exports = router
