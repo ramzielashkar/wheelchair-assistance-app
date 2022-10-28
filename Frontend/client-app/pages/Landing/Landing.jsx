7f7780e9a68b936a898949a768bfb209d6d5b183
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-28 17:37:13 +0300

    Frontend: Including register form

--- Frontend/client-app/pages/Landing/Landing.jsx
+++ Frontend/client-app/pages/Landing/Landing.jsx
@@ -1,11 +1,26 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
 import styles from './styles';
 import logo from '../../assets/images/logo.jpg';
+import Input from '../../components/Inputs/Inputs';
 const Landing = () =>{
     return(
         <View style={styles.root}>
             <View>
-                <Image source={logo}></Image>
+            <View style={styles.container}>
+                <Image style={styles.image} source={require('../../assets/images/logo.jpg')}></Image>
+            </View>
+            <Input 
+            label={"Name"}
+            placeholder={"Enter your name:"}
+            type={"default"}/>
+            <Input 
+            label={"Email"}
+            placeholder={"Enter your email:"}
+            type ={"email-address"}/>
+            <Input 
+            label={"Password"}
+            placeholder={"Enter your password:"}
+            type={"password"}/>
             </View>
         </View>
     );
