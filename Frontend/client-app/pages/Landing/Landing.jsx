72dc3e7a8d5282762e039f98949dd2decbeff415
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-28 18:57:51 +0300

    Frontend: Adding login option

--- Frontend/client-app/pages/Landing/Landing.jsx
+++ Frontend/client-app/pages/Landing/Landing.jsx
@@ -7,23 +7,25 @@ const Landing = () =>{
     return(
         <View style={styles.root}>
             <View>
-            <View style={styles.container}>
-                <Image style={styles.image} source={require('../../assets/images/logo.jpg')}></Image>
-            </View>
-            <Input 
-            label={"Name"}
-            placeholder={"Enter your name:"}
-            type={"default"}/>
-            <Input 
-            label={"Email"}
-            placeholder={"Enter your email:"}
-            type ={"email-address"}/>
-            <Input 
-            label={"Password"}
-            placeholder={"Enter your password:"}
-            type={"password"}/>
-            <Buttons
-            title={"REGISTER"}/>
+                <View style={styles.container}>
+                    <Image style={styles.image} source={require('../../assets/images/logo.jpg')}></Image>
+                </View>
+                <Input 
+                label={"Name"}
+                placeholder={"Enter your name:"}
+                type={"default"}/>
+                <Input 
+                label={"Email"}
+                placeholder={"Enter your email:"}
+                type ={"email-address"}/>
+                <Input 
+                label={"Password"}
+                placeholder={"Enter your password:"}
+                type={"password"}/>
+                <Buttons
+                title={"REGISTER"}/>
+
+                <Text style={styles.text}>Already have an account? SignIn</Text>
             </View>
         </View>
     );
