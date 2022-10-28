0b46c7559122fc8b0cd1253a049be00bdf8c4d2d
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-28 20:33:08 +0300

    Frontend: Styling login page

--- Frontend/client-app/App.js
+++ Frontend/client-app/App.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Landing from './pages/Landing/Landing';
 import Login from './pages/Login/Login';
+import Home from './pages/Home/Home';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+const Tabs = createBottomTabNavigator();
 const AuthStack = createStackNavigator();
 export default function App() {
   return (
@@ -13,6 +16,9 @@ export default function App() {
         <AuthStack.Screen name = 'Register' component={Landing}/>
         <AuthStack.Screen name = 'Login' component={Login}/>
       </AuthStack.Navigator>
+      <Tabs.Navigator>       
+        <Tabs.Screen name = 'Home'/>
+      </Tabs.Navigator>
     </NavigationContainer>  
   );
 }
