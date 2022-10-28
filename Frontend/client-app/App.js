9955f3a0cbc41b670823e20a278d20520388b37a
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-28 19:45:29 +0300

    Frontend: Creating Auth navigation

--- Frontend/client-app/App.js
+++ Frontend/client-app/App.js
@@ -1,20 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Landing from './pages/Landing/Landing';
+import Login from './pages/Login/Login';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
+const AuthStack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AuthStack.Navigator>
+        <AuthStack.Screen name = 'Register' component={Landing}/>
+        <AuthStack.Screen name = 'Login' component={Login}/>
+      </AuthStack.Navigator>
+    </NavigationContainer>  
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
