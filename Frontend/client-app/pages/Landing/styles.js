81cbbb760b5ddd5d3315d92f2f25c3794f80b1cd
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-28 17:37:32 +0300

    Frontend: Styling register form

--- Frontend/client-app/pages/Landing/styles.js
+++ Frontend/client-app/pages/Landing/styles.js
@@ -2,10 +2,28 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     root:{
+        backgroundColor:"#F9F9F9",
+        flex:1,
+        flexDirection:"column",
         alignItems:"center",
-        justifyContent:"center",
-        backgroundColor:'red'
+        justifyContent:"center",    
+        },
+    container:{
+        alignSelf:"center",
     },
+    image:{
+        width:250,
+        height:250,
+        borderRadius:30
+    },
+    user_info:{
+        marginTop:30,
+    },
+    labels:{
+        fontFamily:"Roboto",
+        fontSize:"16",
+        fontWeight:"bold",
+    }
 });
 
 export default styles;
