70ce993da15494aa546d7a6768738cc18d234924
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-28 19:55:11 +0300

    Frontend: Updating login page

--- Frontend/client-app/pages/Login/Login.jsx
+++ Frontend/client-app/pages/Login/Login.jsx
@@ -1,7 +1,27 @@
-import { Text } from "react-native";
+import { StyleSheet, Text, View, Image } from 'react-native';
+import styles from './styles';
+import Input from '../../components/Inputs/Inputs';
+import Buttons from '../../components/Button/Button';
 const Login = () =>{
     return(
-        <Text>Login</Text>
+        <View style={styles.root}>
+            <View>
+                <View style={styles.container}>
+                    <Image style={styles.image} source={require('../../assets/images/logo.jpg')}></Image>
+                </View>
+                <Input 
+                label={"Email"}
+                placeholder={"Enter your email:"}
+                type ={"email-address"}/>
+                <Input 
+                label={"Password"}
+                placeholder={"Enter your password:"}
+                type={"password"}/>
+                <Buttons
+                title={"LOGIN"}/>
+
+            </View>
+        </View>
     );
 }
 
