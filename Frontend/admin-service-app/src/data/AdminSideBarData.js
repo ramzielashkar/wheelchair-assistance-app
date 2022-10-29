b79776c1f7f641d69a3260ab6865149ba6560aef
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-29 21:28:57 +0300

    Frontend: Updating admin sidebar data

--- Frontend/admin-service-app/src/data/AdminSideBarData.js
+++ Frontend/admin-service-app/src/data/AdminSideBarData.js
@@ -2,17 +2,17 @@ import {MdHomeRepairService,MdOutlineGroups, MdBarChart} from "react-icons/md";
 export const AdminSideBarData =[
     {
         title : "Service Providers",
-        path:"/",
+        path:"/admin/services",
         icon :<MdHomeRepairService size={30}/>
     },
     {
         title : "Clients",
-        path:"/",
+        path:"/admin/clients",
         icon :<MdOutlineGroups size={30}/>
     },
     {
         title : "Statistics",
-        path:"/",
+        path:"/admin/stats",
         icon :<MdBarChart size={30}/>
     }
 ]
