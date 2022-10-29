e209a1b465a60d65e4541125875ccec82855bdcb
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-29 16:38:02 +0300

    Frontend: Including login form

--- Frontend/admin-service-app/src/pages/Landing/Landing.jsx
+++ Frontend/admin-service-app/src/pages/Landing/Landing.jsx
@@ -1,14 +1,36 @@
 import './style.css';
 import logo from '../../assets/images/logo.jpg';
+import Input from '../../components/Input/Input';
+import { useState } from 'react';
+import { useEffect } from 'react';
 const Landing =()=>{
+    const [error, setError]= useState(false);
+    let errorfield;
+    if(error){
+         errorfield = <p className='error '>Invalid Credentials</p>
+    }else{
+         errorfield = <p className='error hidden'>Invalid Credentials</p>
+    }
     return(
         <section className="root flex column">
             <div className="container flex">
-                <div className="logo">
+                <div className="logo-container">
                     <img src={logo} className="logo-image"/>
                 </div>
                 <form className='login-form flex column'>
-                    hello
+                    <Input
+                    type={'email'}
+                    placeholder={"Enter your email"}
+                    name={"landing-input"}
+                    label={"Email"}/>
+                    <Input
+                    type={'password'}
+                    placeholder={"Enter your password"}
+                    name={"landing-input"}
+                    label={"Password"}/>  
+                    {errorfield}
+                    <input type={"submit"} value="LOGIN" className="btn-login" onClick={()=>{setError(true)}}/>
+
                 </form>
             </div>
         </section>
