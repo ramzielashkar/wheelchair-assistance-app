0c3a94c4bfe9f7757dd60f3f9b742a61c1ea8e93
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-29 22:46:44 +0300

    Frontend: Including add new service component

--- Frontend/admin-service-app/src/pages/Admin-ServiceProviders/AdminServices.jsx
+++ Frontend/admin-service-app/src/pages/Admin-ServiceProviders/AdminServices.jsx
@@ -2,10 +2,18 @@ import { Outlet, useNavigate } from 'react-router-dom';
 import Button from '../../components/Button/Button';
 import { useState } from 'react';
 import './style.css';
+import NewService from '../../components/NewService/NewService';
 const AdminServices = ()=>{
     const navigate = useNavigate();
     const [active, setActive] = useState(true);
     const [banned, setBanned]=useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+    const showAddService = ()=>{
+        setIsOpen(true);
+    }
+    const closeAddService = ()=>{
+        setIsOpen(false);
+    }
     const showBanned = ()=>{
         setBanned(true);
         setActive(false);
@@ -28,8 +36,12 @@ const AdminServices = ()=>{
                      onClick={showBanned}>Banned</p>
                 </div>
                 <Button
-                text={"Add Service Provider"}/>
+                text={"Add Service Provider"}
+                onClick={showAddService}/>
             </div>
+            <NewService
+            isOpen={isOpen}
+            onClose={closeAddService}/>
             <Outlet/>
         </section>
     );
