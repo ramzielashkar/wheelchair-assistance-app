f86f8f25fca859bad4846411f479f3023e3d0797
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-29 19:40:07 +0300

    Frontend: Creating admin sidebar data

---
+++ Frontend/admin-service-app/src/data/AdminSideBarData.js
@@ -0,0 +1,18 @@
+import {MdHomeRepairService,MdOutlineGroups, MdBarChart} from "react-icons/md";
+export const AdminSideBarData =[
+    {
+        title : "Service Providers",
+        path:"/",
+        icon :<MdHomeRepairService size={30}/>
+    },
+    {
+        title : "Clients",
+        path:"/",
+        icon :<MdOutlineGroups size={30}/>
+    },
+    {
+        title : "Statistics",
+        path:"/",
+        icon :<MdBarChart size={30}/>
+    }
+]
