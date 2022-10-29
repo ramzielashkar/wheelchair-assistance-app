ae9ab3b32ca356cecab7c169d7c1a2c1c5b233bf
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-29 22:45:18 +0300

    Frontend: Adding select input

--- Frontend/admin-service-app/src/components/Input/Input.jsx
+++ Frontend/admin-service-app/src/components/Input/Input.jsx
@@ -1,11 +1,24 @@
 import './styles.css';
 const Input = ({name, placeholder, onChange, label, type })=> {
+    if(type=="select"){
+        return(
+            <>
+            <label htmlFor="email">{label}</label>
+            <select className={name} name="type">
+                <option value={"Hospital"}>Hospital</option>
+                <option value={"Vendor"}>Vendor</option>
+                <option value={"Restaurant"}>Restaurant</option>
+            </select>
+            </>
+        );
+    }else{
     return(
         <>
         <label htmlFor="email">{label}</label>
         <input type={type} placeholder={placeholder} className={name} id="email"/>
         </>
     );
+    }
 }
 
 export default Input;
