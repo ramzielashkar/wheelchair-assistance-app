8545c4dea4cf95c71a69999e51af89f0d1a524f7
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-10-31 20:37:07 +0200

    Frontend: Including new notification popup component

--- Frontend/admin-service-app/src/pages/ServiceNotifications/ServiceNotifications.jsx
+++ Frontend/admin-service-app/src/pages/ServiceNotifications/ServiceNotifications.jsx
@@ -2,18 +2,28 @@ import Button from '../../components/Button/Button';
 import './style.css';
 import {MdAdd} from 'react-icons/md';
 import NotificationCard from '../../components/NotificationCard/NotificationCard';
+import NewNotification from '../../components/NewNotification/NewNotification';
+import { useState } from 'react';
 
 const ServiceNotifications = ()=>{
+    const [isOpen, setIsOpen] = useState(false);
+    const showAddNotification=()=>{
+        setIsOpen(true);
+    } 
+    const closeAddNotification=()=>{
+        setIsOpen(false);
+    } 
     return(
         <section className="flex column notifications-section">
         <div className="notifications-header flex">
                 <p className="notifications-title">Notifications</p>
                 <div className="add">
                 <Button
+                onClick={showAddNotification}
                 text={"New Notification"}/>
                 </div>
                 
-                <div className="add-btn" onClick={""}>
+                <div className="add-btn" onClick={showAddNotification}>
                         <MdAdd size={40} color={"white"}/>
                 </div>
         </div>
@@ -21,6 +31,10 @@ const ServiceNotifications = ()=>{
         <NotificationCard/>
         <NotificationCard/>
         <NotificationCard/>
+
+        <NewNotification
+        isOpen={isOpen}
+        onClose={closeAddNotification}/>
 
     </section>
     );
