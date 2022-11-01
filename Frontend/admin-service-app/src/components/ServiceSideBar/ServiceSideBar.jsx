329b78cd5c9c209c4658e897f184c04277aa0632
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-01 13:04:00 +0200

    Frontend: Updating service provider sidebar

--- Frontend/admin-service-app/src/components/ServiceSideBar/ServiceSideBar.jsx
+++ Frontend/admin-service-app/src/components/ServiceSideBar/ServiceSideBar.jsx
@@ -3,6 +3,7 @@ import logo from '../../assets/images/logo.jpg';
 import { ServiceSideBarData } from '../../data/ServiceSideBarData';
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { MdMoreVert } from "react-icons/md";
 const ServiceSideBar = () =>{
         const [logoutPopup, setLogoutPopup] = useState(false);
         let logoutContainer;
@@ -24,11 +25,15 @@ const ServiceSideBar = () =>{
                         </NavLink>
                     );
                 })}
-                <div className='flex profile-item' onClick={()=>{{setLogoutPopup(!logoutPopup)}}}>
-                    <img className='profile-img' src={logo} alt="" width={40} height={40} />
-                    <p className='username'>Ramzi El Ashkar</p>
-                    {logoutContainer}
+                <div className="flex column sidebar-end">
+                    <div className='flex profile-item' onClick={()=>{{setLogoutPopup(!logoutPopup)}}}>
+                        <img className='profile-img' src={logo} alt="" width={40} height={40} />
+                        <p className='username'>Ramzi El Ashkar</p>
+                        {logoutContainer}
+                        <MdMoreVert color='#3a3a3a' size={30}/>
+                    </div>
                 </div>
+                
                 
             </div>
         );
