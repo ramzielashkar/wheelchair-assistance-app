3c26c0710314a925cb59f3594f9c3c21c258c4ef
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-03 12:03:29 +0200

    Frontend: Updating stack switcher

--- Frontend/client-app/navigation/StackSwitcher.jsx
+++ Frontend/client-app/navigation/StackSwitcher.jsx
@@ -2,6 +2,7 @@ import TabsStack from "./TabsStack";
 import AuthStack from "./AuthStack";
 import { useState } from "react";
 
+// stack switcher based on user
 const StackSwitcher = ()=>{
     const [user, setUser] = useState(true);
     return user? <TabsStack/> : <AuthStack/>
