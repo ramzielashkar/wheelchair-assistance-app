acceaf34d58fe75357a1f03c0150925d45c1af99
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-03 13:08:54 +0200

    Frontend: Updating navigations

--- Frontend/client-app/App.js
+++ Frontend/client-app/App.js
@@ -1,26 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Landing from './pages/Landing/Landing';
-import Login from './pages/Login/Login';
-import Home from './pages/Home/Home';
-import Profile from './pages/Profile/Profile';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import StackSwitcher from './navigation/StackSwitcher';
+import TabsStack from './navigation/TabsStack';
 
-const Tabs = createBottomTabNavigator();
-const AuthStack = createStackNavigator();
+
 export default function App() {
   return (
-    <NavigationContainer>
-     {/*<AuthStack.Navigator>
-        <AuthStack.Screen name = 'Register' component={Landing}/>
-        <AuthStack.Screen name = 'Login' component={Login}/>
-  </AuthStack.Navigator>*/}
-      <Tabs.Navigator>       
-        <Tabs.Screen name = 'Home' component={Home}/>
-        <Tabs.Screen name = 'Profile' component={Profile}/>
-      </Tabs.Navigator>
-    </NavigationContainer>  
-  );
+    <StackSwitcher/>
+    );
 }
