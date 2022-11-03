1d2a8948f0cc721b6df08f2ed04bdf3e1b13cf80
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-03 12:02:59 +0200

    Frontend: Updating bottom tab stack navigation

--- Frontend/client-app/navigation/TabsStack.jsx
+++ Frontend/client-app/navigation/TabsStack.jsx
@@ -3,16 +3,60 @@ import { createStackNavigator } from "@react-navigation/stack";
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from "../pages/Home/Home";
 import Profile from "../pages/Profile/Profile";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import TopBar from "../components/TopBar/TopBar";
+import  Icon  from '@expo/vector-icons/FontAwesome5';
+
+
+// Bottom tab stack navigator
 const TabsStack = ()=>{
     const Tabs = createBottomTabNavigator();
     return (
         <NavigationContainer>
           <Tabs.Navigator 
-          tabBarOptions={{
-            showLabel: false,
-          }}>       
-            <Tabs.Screen name = 'Home' component={Home} />
-            <Tabs.Screen name = 'Profile' component={Profile}/>
+            initialRouteName="Home"
+            screenOptions={{
+                "tabBarHideOnKeyboard": true,
+                "tabBarActiveTintColor": "#0A61E1",
+                "tabBarInactiveTintColor": "rgba(0, 0, 0, 0.8)",
+                "tabBarAllowFontScaling": false,
+                "tabBarShowLabel": true,
+                "tabBarLabelStyle": {
+                    "fontWeight": "bold",
+                    "fontSize": 10
+                },
+            }}
+            /*tabBarOptions={{
+                activeTintColor: '#0A61E1',
+                inactiveTintColor: 'rgba(0, 0, 0, 0.8)',
+                showLabel: true,
+                allowFontScaling: false,
+                keyboardHidesTabBar: true,
+                shadowColor: '#FFF',
+                labelStyle: {
+                  fontWeight: "bold",
+                  fontSize: 10,
+                },
+              }}*/>       
+            <Tabs.Screen name = 'Home' component={Home}
+             options={{
+            title: 'Home',
+            headerTitle: (props) => <TopBar {...props} />,
+            tabBarIcon: ({ focused, color, size }) => (
+              <MaterialCommunityIcons
+                name={"home"}
+                size={28}
+                color={color}
+              />
+            ),
+          }} />
+            <Tabs.Screen name = 'Profile' component={Profile}
+            options={{
+            headerTitle: (props) => <TopBar {...props} />,
+            tabBarIcon: ({ focused, color, size }) => (
+                <Icon name="user-alt" size={20} color={color}/>           
+               ),
+          }} />
           </Tabs.Navigator>
         </NavigationContainer>  
       );
