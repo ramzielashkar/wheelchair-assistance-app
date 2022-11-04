e6134f0862f4a5984e74e649918835ebcf242aed
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-04 11:28:03 +0200

    Frontend: Creating item separator component

---
+++ Frontend/client-app/components/ItemSeparator/ItemSeparator.jsx
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import styles from './styles';
+
+const ItemSeparator = ()=>{
+    return(
+        <View style={styles.separator}>
+        </View>
+    );
+}
+export default ItemSeparator;
