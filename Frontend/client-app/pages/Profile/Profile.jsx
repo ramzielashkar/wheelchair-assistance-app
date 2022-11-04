11a0a26b511a4d276dcce44a6e61f7235e7e4cbd
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-04 14:01:51 +0200

    Frontend: Creating edit profile section

--- Frontend/client-app/pages/Profile/Profile.jsx
+++ Frontend/client-app/pages/Profile/Profile.jsx
@@ -1,29 +1,39 @@
-import { Text, View,Dimensions, ScrollView, FlatList, Image } from "react-native";
+import { Text, View,Dimensions, ScrollView, FlatList, Image, TextInput } from "react-native";
 import styles from "./styles";
 import { AntDesign, Ionicons } from '@expo/vector-icons'; 
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { useState } from "react";
 
 const Profile = () =>{
     let screenWidth = Dimensions.get('window').width
     let flatListStyle;
+    const [editable, setEditable]= useState(false);
     const edit =()=>{
-        console.log('edit')
+        setEditable(true)
     }
-
+    const save =()=>{
+        console.log('save')
+    }
     return(
         <ScrollView style={styles.root}>
             <View style={styles.imageContainer}>
                 <Image style={styles.image} source={require('../../assets/images/hospital.webp')}></Image>
             </View>
             <View style={styles.profileContainer}>
-                <Text style={styles.userName}>Ramzi El Ashkar</Text>
+                <TextInput 
+                style={
+                    !editable? styles.userName : styles.editable } 
+                    keyboardType="default" editable={editable} defaultValue="Ramzi El Ashkar"></TextInput>
                 <View style={styles.location}>
                     <View style={styles.locationContainer}>
                         <Ionicons name="location-outline" size={15} color="#0A61E1" />
                         <Text style={styles.locationText}>Beirut, Lebanon</Text>
                     </View>
                     <View>
-                        <Ionicons name="ios-pencil-sharp" size={20} color="#0A61E1" onPress={edit} />
+                        <Ionicons name={
+                            !editable? "ios-pencil-sharp": "checkmark-sharp"
+                        } size={20} color="#0A61E1" onPress={
+                            !editable? edit : save } />
                     </View>
                 </View>
                 <View style={styles.location}>
@@ -33,7 +43,7 @@ const Profile = () =>{
                     </View>
                     
                 </View>
-        </View>
+            </View>
         </ScrollView>
     );
 
