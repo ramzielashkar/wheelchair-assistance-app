a29ab2c5f545f013bbb01a63532878db417bfb5d
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-04 21:14:19 +0200

    Frontend: Creating favorites stack navigation

---
+++ Frontend/client-app/navigation/FavoritesStack.jsx
@@ -0,0 +1,28 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import TopBar from "../components/TopBar/TopBar";
+import ServicePage from "../pages/ServicePage/ServicePage";
+import Favorites from '../pages/Favorites/Favorites';
+
+const FavoritesStack = ()=>{
+    const FavoritesStack = createStackNavigator();
+    return (
+        <FavoritesStack.Navigator
+        initialRouteName="Favorites">
+            <FavoritesStack.Screen name="Favorites" component={Favorites} 
+            options={{
+                title: 'Favorites',
+                headerTitle: (props) => <TopBar props={'Favorites'} />
+                }}/> 
+             <FavoritesStack.Screen name="Service" component={ServicePage}
+                options={({ route }) => ({ title: route.params.name,
+                    headerTitle: (props) => <TopBar props={route.params.name}  path={"stack"}/>
+                })}
+
+             /> 
+
+        </FavoritesStack.Navigator>
+       );
+}
+export default FavoritesStack;
