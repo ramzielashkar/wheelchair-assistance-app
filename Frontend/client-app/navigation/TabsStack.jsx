e012283a156e04db907535adca3bc66017a92a95
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-04 12:31:46 +0200

    Frontend: Creating search page navigation

--- Frontend/client-app/navigation/TabsStack.jsx
+++ Frontend/client-app/navigation/TabsStack.jsx
@@ -7,6 +7,7 @@ import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityI
 import TopBar from "../components/TopBar/TopBar";
 import  Icon  from '@expo/vector-icons/FontAwesome5';
 import Favorites from "../pages/Favorites/Favorites";
+import Search from "../pages/Search/Search";
 
 
 // Bottom tab stack navigator
@@ -51,13 +52,7 @@ const TabsStack = ()=>{
               />
             ),
           }} />
-            <Tabs.Screen name = 'Profile' component={Profile}
-            options={{
-            headerTitle: (props) => <TopBar props={'Profile'} />,
-            tabBarIcon: ({ focused, color, size }) => (
-                <Icon name="user-alt" size={20} color={color}/>           
-               ),
-          }} />
+            
           <Tabs.Screen name = 'Favorites' component={Favorites}
             options={{
             headerTitle: (props) => <TopBar props={'Favorites'} />,
@@ -67,6 +62,20 @@ const TabsStack = ()=>{
               size={26}
               color={color}
             />               ),
+          }} />
+          <Tabs.Screen name = 'Search' component={Search}
+            options={{
+            headerTitle: (props) => <TopBar props={'Search'} />,
+            tabBarIcon: ({ focused, color, size }) => (
+              <Icon name="search" size={20} color={color}/>           
+              ),
+          }} />
+          <Tabs.Screen name = 'Profile' component={Profile}
+            options={{
+            headerTitle: (props) => <TopBar props={'Profile'} />,
+            tabBarIcon: ({ focused, color, size }) => (
+                <Icon name="user-alt" size={20} color={color}/>           
+               ),
           }} />
           </Tabs.Navigator>
         </NavigationContainer>  
