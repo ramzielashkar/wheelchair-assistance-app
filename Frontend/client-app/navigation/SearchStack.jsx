93032d9f5e92380ae404012d1e905e36cd98d7ce
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-04 21:18:31 +0200

    Frontend: Creating Search stack navigation

---
+++ Frontend/client-app/navigation/SearchStack.jsx
@@ -0,0 +1,27 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import TopBar from "../components/TopBar/TopBar";
+import ServicePage from "../pages/ServicePage/ServicePage";
+import Search from "../pages/Search/Search";
+const SearchStack = ()=>{
+    const SearchStack = createStackNavigator();
+    return (
+        <SearchStack.Navigator
+        initialRouteName="Favorites">
+            <SearchStack.Screen name="Search" component={Search} 
+            options={{
+                title: 'Search',
+                headerTitle: (props) => <TopBar props={'Search'} />
+                }}/> 
+             <SearchStack.Screen name="Service" component={ServicePage}
+                options={({ route }) => ({ title: route.params.name,
+                    headerTitle: (props) => <TopBar props={route.params.name}  path={"stack"}/>
+                })}
+
+             /> 
+
+        </SearchStack.Navigator>
+       );
+}
+export default SearchStack;
