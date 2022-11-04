9ecb584dad11cbbf5176b35f631fe130aa366f90
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-04 17:05:27 +0200

    Frontend: Styling empty state component

---
+++ Frontend/client-app/components/EmptyState/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native"
+const styles = StyleSheet.create({
+    EmptySection:{
+        flex:1,
+        flexDirection:"column",
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    empty:{
+        flex:1,
+        flexDirection:"column",
+        justifyContent:"center",
+        alignItems:"center",
+    },
+    text:{
+        fontFamily:"Roboto",
+        marginTop:10
+
+    }
+})
+
+export default styles;
