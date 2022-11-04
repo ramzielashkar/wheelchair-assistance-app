38e13a6326905e974c218ecf98ab10961d82aeb6
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-04 11:25:49 +0200

    Frontend: Updating favorites page

--- Frontend/client-app/pages/Favorites/Favorites.jsx
+++ Frontend/client-app/pages/Favorites/Favorites.jsx
@@ -1,19 +1,42 @@
-import { Text, View,Dimensions, ScrollView } from "react-native";
+import { Text, View,Dimensions, ScrollView, FlatList } from "react-native";
 import ServiceCard from "../../components/ServiceCard/ServiceCard";
 import styles from "./style";
 
+const data = [
+    {
+        name:"Restaurant",
+        location:"Beirut, Lebanon"
+},
+{
+    name:"Restaurant",
+    location:"Beirut, Lebanon"
+},
+{
+    name:"Restaurant",
+    location:"Beirut, Lebanon"
+},
+]
 const Favorites =()=>{
+    let screenWidth = Dimensions.get('window').width
+ console.log(screenWidth);
+ let columns=2;
+ if(screenWidth > 600){
+    columns=3;
+ }
+ 
     return(
         <ScrollView style={styles.root}>
             <View style={styles.serviceContainer}>
-                <View style={styles.services}>
-                <ServiceCard 
-                    name={'Restaurant'}
-                    location={'Beirut, Lebanon'}/>
-                    <ServiceCard 
-                    name={'Restaurant'}
-                    location={'Beirut'}/>
-                </View>
+            <FlatList 
+                    columnWrapperStyle={{gap:20, marginBottom:20}}
+                    data={data}
+                    renderItem={({item})=>(
+                        <ServiceCard
+                        data={item}/>
+                    )}
+                    numColumns={columns}
+                    >
+                </FlatList>
             </View>
         </ScrollView>
     );
