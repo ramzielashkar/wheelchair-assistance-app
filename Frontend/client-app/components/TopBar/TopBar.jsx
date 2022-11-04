53b70ddc3b122e07aa7c700672f6d3bc809bbe39
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-04 11:27:36 +0200

    Frontend: Updating topbar component

--- Frontend/client-app/components/TopBar/TopBar.jsx
+++ Frontend/client-app/components/TopBar/TopBar.jsx
@@ -3,12 +3,12 @@ import styles from './styles';
 import logo from '../../assets/images/logo.jpg';
 import  Icon  from '@expo/vector-icons/Ionicons';
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-const TopBar = ()=>{
+const TopBar = ({props})=>{
     return(
         <View style={styles.headerContainer}>
             <View style={styles.titleContainer}>
                 <Image source={logo} style={styles.logoImage}></Image>
-                <Text style={styles.title}>Wheel of Life</Text>
+                <Text style={styles.title}>{props}</Text>
             </View>
             <View style={styles.navigationContainer}>
         <Icon name="notifications-outline" size={28} color={"#0A61E1"} />
