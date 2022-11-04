7f444df4586a3d863ec10e297ac037235c555e63
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-04 20:00:32 +0200

    Frontend: Updating home stack navigation

--- Frontend/client-app/navigation/HomeStack.jsx
+++ Frontend/client-app/navigation/HomeStack.jsx
@@ -4,23 +4,29 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from "../pages/Home/Home";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import TopBar from "../components/TopBar/TopBar";
-import Hospitals from "../pages/Hospitals/Hospitals";
+import Services from "../pages/Hospitals/Services";
+import ServicePage from "../pages/ServicePage/ServicePage";
 
 const HomeStack = ()=>{
     const homeStack = createStackNavigator();
     return (
         <homeStack.Navigator
-       >
+        initialRouteName="HomePage">
             <homeStack.Screen name="HomePage" component={Home} 
             options={{
                 title: 'Home',
                 headerTitle: (props) => <TopBar props={'Wheel of Life'} />
                 }}/> 
-            <homeStack.Screen name="Hospitals" component={Hospitals}
-             options={{
-                title: 'Home',
-                headerTitle: (props) => <TopBar props={'Hospitals'}  path={"stack"}/>
-                }}
+            <homeStack.Screen name="Services" component={Services}
+             options={({ route }) => ({ title: route.params.type,
+                headerTitle: (props) => <TopBar props={route.params.type}  path={"stack"}/>
+            })}
+             /> 
+             <homeStack.Screen name="Service" component={ServicePage}
+                options={({ route }) => ({ title: route.params.name,
+                    headerTitle: (props) => <TopBar props={route.params.name}  path={"stack"}/>
+                })}
+
              /> 
 
         </homeStack.Navigator>
