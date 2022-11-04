32ce938395b0d9ac6cef4f4c5c34b73bad305222
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-04 16:42:13 +0200

    Frontend: Creating edit profile picture section

--- Frontend/client-app/pages/Profile/Profile.jsx
+++ Frontend/client-app/pages/Profile/Profile.jsx
@@ -3,21 +3,54 @@ import styles from "./styles";
 import { AntDesign, Ionicons } from '@expo/vector-icons'; 
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { useState } from "react";
+import photo from '../../assets/images/hospital.webp';
+import * as ImagePicker from 'expo-image-picker';
+
 
 const Profile = () =>{
-    let screenWidth = Dimensions.get('window').width
-    let flatListStyle;
     const [editable, setEditable]= useState(false);
+    const [imageEdit, setImageEdit]= useState(false);
+    const [image, setImage] = useState(null);
+    const [base64, setBase64] = useState(null);
     const edit =()=>{
         setEditable(true)
     }
     const save =()=>{
+        setEditable(false)
         console.log('save')
+    }
+    // function to choose photo
+    const handleChoosePhoto= async()=>{
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+            base64: true,
+          });
+          
+          if (!result.cancelled) {
+            setBase64(result.base64)
+            console.log(result.base64)
+            setImageEdit(true);
+            setImage(result.uri);
+          }
+    }
+    //function to submit photo
+    const submitPhoto =()=>{
+        setImageEdit(false);
+        console.log('submit');
     }
     return(
         <ScrollView style={styles.root}>
             <View style={styles.imageContainer}>
-                <Image style={styles.image} source={require('../../assets/images/hospital.webp')}></Image>
+                <Image style={styles.image} source={{uri:image}}></Image>
+                <View style={styles.editPicture}>
+                    <MaterialCommunityIcons name={
+                        !imageEdit?"image-edit":"check" }
+                         size={40} color={"white"} onPress={
+                            !imageEdit? handleChoosePhoto : submitPhoto}/>
+                </View>
             </View>
             <View style={styles.profileContainer}>
                 <TextInput 
