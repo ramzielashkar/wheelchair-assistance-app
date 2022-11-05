0c44d58234c415f456cdc4fade100395b35286b0
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-05 20:39:13 +0200

    Frontend: Styling chats page

---
+++ Frontend/client-app/pages/Chats/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    root:{
+        backgroundColor:"#F9F9F9",
+        flex:1,
+        flexDirection:"column",
+        },
+        chatsContainer:{
+            marginBottom:12,
+            shadowColor: "#000",
+            shadowOffset: {
+                width: 0,
+                height: 6,
+            },
+            shadowOpacity: 0.39,
+            shadowRadius: 8.30,
+            elevation: 13,
+        }
+})
+export default styles;
