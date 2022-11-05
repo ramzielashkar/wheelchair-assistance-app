0fed1d9c37872cdfc975c327d8d82880e9e20690
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-05 19:20:19 +0200

    Frontend: Updating map page

--- Frontend/client-app/pages/Map/Map.jsx
+++ Frontend/client-app/pages/Map/Map.jsx
@@ -3,9 +3,22 @@ import MapView,{PROVIDER_GOOGLE, Marker, LatLng} from 'react-native-maps';
 import styles from "./styles";
 import { GOOGLE_API_KEY } from "../../Credentials/credentials";
 import { useState } from "react";
+import MapViewDirections from 'react-native-maps-directions';
+import Buttons from "../../components/Button/Button";
+
 const Map = ()=>{
     const [location, setLocation]= useState('');
+    const [showDirections, setShowDirections]=useState(false);
     const {width, height} = Dimensions.get("window");
+    const origin = {
+        latitude : 33.793791,
+        longitude: 35.401778
+    }
+    const dest = {
+        latitude : 33.952141,
+        longitude: 35.592972
+    }
+
     const ASPECT_RATIO = width/height;
     const LATITUDE_DELTA = 0.02;
     const LONGITUDE_DELTA =LATITUDE_DELTA*ASPECT_RATIO;
@@ -26,13 +39,19 @@ const Map = ()=>{
                     latitude : 33.893791,
                     longitude: 35.501778
                 }}/>
-                <Marker coordinate={{
-                    latitude : 33.793791,
-                    longitude: 35.401778
-                }}/>
+                <Marker coordinate={origin}/>
+                <MapViewDirections 
+                origin={origin}
+                destination={dest}
+                apikey={GOOGLE_API_KEY}/>
                 </MapView>
-                
+                <View style={styles.btn}>
+                    <Buttons
+                    title={'SHOW DIRECTIONS'}
+                    onClick={()=>{setShowDirections(true)}}/>
+                </View>
             </View>
+            
         </View>
         );
 }
--- Frontend/client-app/pages/Map/styles.js
+++ Frontend/client-app/pages/Map/styles.js
@@ -7,6 +7,7 @@ const styles = StyleSheet.create({
         flexDirection:"column",
         },
     mapContainer:{
+        position:"relative",
         borderRadius:30,
         height:"100%",
         width:"100%"
@@ -15,6 +16,14 @@ const styles = StyleSheet.create({
         width:'100%',
         height:"100%",
         borderRadius:30,
+    },
+    btn:{
+        position:"absolute",
+        left:30,
+        bottom:20,
+        flex:1,
+        flexDirection:"row",
+        justifyContent:"flex-end"
     }
 })
 
