f121d7e60780ef05579ae5ae36d883125371a731
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-05 15:13:03 +0200

    Frontend: Updating service page navigation

--- Frontend/client-app/pages/ServicePage/ServicePage.jsx
+++ Frontend/client-app/pages/ServicePage/ServicePage.jsx
@@ -1,16 +1,22 @@
-import { Text, View,Dimensions, ScrollView, FlatList, Image, TextInput, TouchableOpacity } from "react-native";
+import { Text, View,Dimensions, ScrollView, FlatList, Image, TextInput, TouchableOpacity, Button } from "react-native";
 import styles from "./styles";
 import { AntDesign, Ionicons } from '@expo/vector-icons'; 
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { useState } from "react";
 import photo from '../../assets/images/hospital.webp';
 import { ImageSlider } from "react-native-image-slider-banner";
+import Buttons from "../../components/Button/Button";
 
 
 const ServicePage = ({navigation, route})=>{
     const [image, setImage] = useState(photo);
     const [followed, setfollowed] = useState(false);
 
+    //function to navigate to map
+    const navigateToMap=()=>{
+        console.log("map")
+        navigation.navigate('Map', {name:route.params.name})
+    }
 const data=[
 {   img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYNCb0fpwqIyI2_kVF10edAAxN7vuL1XBmNw&usqp=CAU",
 },   
@@ -65,7 +71,10 @@ const data=[
                 </View>
                 <Text style={styles.hours}>Working Hours</Text>
                 <Text style={styles.workingHours}>Monday to Friday 10am to 11pm</Text>
-
+                <Buttons 
+                title={"SHOW ON MAP"}
+                onClick={navigateToMap}
+                />
             </View>
         </ScrollView>
     );
