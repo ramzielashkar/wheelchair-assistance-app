c2efe6d785cac38d64fec84746abb87b6733bf74
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-05 15:12:08 +0200

    Frontend: Styling map page

---
+++ Frontend/client-app/pages/Map/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    root:{
+        backgroundColor:"#F9F9F9",
+        flex:1,
+        flexDirection:"column",
+        },
+    mapContainer:{
+        borderRadius:30,
+        height:"100%",
+        width:"100%"
+    },
+    map:{
+        width:'100%',
+        height:"100%",
+        borderRadius:30,
+    }
+})
+
+export default styles;
