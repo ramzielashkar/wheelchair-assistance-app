408473e65c55d43c5559f748c7cbfe32f628afa1
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-05 20:39:01 +0200

    Frontend: Creating chats page

---
+++ Frontend/client-app/pages/Chats/Chats.jsx
@@ -0,0 +1,49 @@
+import { Text, ScrollView, FlatList, View } from "react-native";
+import ChatCard from "../../components/ChatCard/ChatCard";
+import styles from "./styles";
+const data = [
+    {
+        img:  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnJyBH7aR8qVizMo5qDuRnGJu8dn_uEmF3EA&usqp=CAU",
+        name:"Ramzi",
+        content:"Hello, how are you",
+        date:"Oct,10, 12:00PM"
+
+    },
+    {
+        img:  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnJyBH7aR8qVizMo5qDuRnGJu8dn_uEmF3EA&usqp=CAU",
+        name:"Marwan",
+        content:"Hello, how are youfdfdf dfdfdf dfdfdf dfdfdf dfdfdf ",
+        date:"Oct,10, 12:00PM"
+
+    },
+    {
+        img:  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnJyBH7aR8qVizMo5qDuRnGJu8dn_uEmF3EA&usqp=CAU",
+        name:"Karam",
+        content:"Hello, how are you",
+        date:"Oct,10, 12:00PM"
+
+    },
+    {
+        img:  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnJyBH7aR8qVizMo5qDuRnGJu8dn_uEmF3EA&usqp=CAU",
+        name:"Razan",
+        content:"Hello, how are you",
+        date:"Oct,10, 12:00PM"
+
+    }
+]
+const Chats = ()=>{
+    return(
+        <ScrollView style={styles.root}>
+            <View style={styles.chatsContainer}>
+                <FlatList
+                data={data}
+                renderItem={({item})=>(
+                    <ChatCard
+                    data={item}/>
+                )}>
+                </FlatList>
+            </View>
+        </ScrollView>
+        );
+}
+export default Chats;
