283eec123836344790ddb988855f4728c6bee57a
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-06 23:16:30 +0200

    Frontend: Updating input component

--- Frontend/admin-service-app/src/components/Input/Input.jsx
+++ Frontend/admin-service-app/src/components/Input/Input.jsx
@@ -4,7 +4,7 @@ const Input = ({name, placeholder, onChange, label, type, value, disabled})=> {
         return(
             <>
             <label htmlFor="email">{label}</label>
-            <select className={name} name="type">
+            <select className={name} name="type" onChange={onChange}>
                 <option value={"Hospital"}>Hospital</option>
                 <option value={"Vendor"}>Vendor</option>
                 <option value={"Restaurant"}>Restaurant</option>
@@ -15,7 +15,7 @@ const Input = ({name, placeholder, onChange, label, type, value, disabled})=> {
     return(
         <>
         <label htmlFor={placeholder}>{label}</label>
-        <input type={type} placeholder={placeholder} className={name} defaultValue={value} id={placeholder} disabled={disabled}/>
+        <input type={type} placeholder={placeholder} className={name} defaultValue={value} id={placeholder} disabled={disabled} onChange={onChange}/>
         </>
     );
     }
