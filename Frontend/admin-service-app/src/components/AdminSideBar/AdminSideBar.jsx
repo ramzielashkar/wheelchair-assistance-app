441378100e806927b03c9627a7bbbd65be4ce30a
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-06 23:15:58 +0200

    Linking: Displaying user info

--- Frontend/admin-service-app/src/components/AdminSideBar/AdminSideBar.jsx
+++ Frontend/admin-service-app/src/components/AdminSideBar/AdminSideBar.jsx
@@ -4,16 +4,21 @@ import { AdminSideBarData } from '../../data/AdminSideBarData';
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { MdMoreVert } from "react-icons/md";
-
+import { useCurrentUser } from '../../query/auth/auth';
+import { baseUrl } from '../../query/axios/axios';
 const AdminSideBar = () =>{
+    const [user, setUser] = useState('');
     const [logoutPopup, setLogoutPopup] = useState(false);
+     const currentUser  =  useCurrentUser().then((res)=>{
+        setUser(res)
+    });
     let logoutContainer;
     if(logoutPopup){
         logoutContainer = <div className='logoutContainer'>Logout</div>
     }else{
         logoutContainer = <div className='logoutContainer hidden'>Logout</div>
     }
-    return(
+     return(
         <div className="flex column admin-sidebar">
             {AdminSideBarData.map((item, index)=>{
                 return(
@@ -27,17 +32,21 @@ const AdminSideBar = () =>{
                 );
             })}
             <div className="flex column sidebar-end">
-                <div className='flex profile-item' onClick={()=>{{setLogoutPopup(!logoutPopup)}}}>
-                    <img className='profile-img' src={logo} alt="" width={40} height={40} />
-                    <p className='username'>Ramzi El Ashkar</p>
-                    {logoutContainer}
+                {user &&<div className='flex profile-item' onClick={()=>{setLogoutPopup(!logoutPopup)}}>
+                    <img className='profile-img' src={`${baseUrl}/public/${user.data.user.profile_picture}`} alt="" width={40} height={40} />
+                    <p className='username'>{user.data.user.name}</p>
+                    <div className={
+                        logoutPopup? "logoutContainer" : "hidden"
+                    }>Logout</div>
                     <MdMoreVert className='more' color='#3a3a3a' size={30}/>
                 </div>
+                }
             </div>
            
             
         </div>
     );
+        
 }
 
 export default AdminSideBar;
