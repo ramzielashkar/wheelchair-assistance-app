ad597b1fe75c371d157797aa67886a464fa19899
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-06 23:17:53 +0200

    Linking: Creating login function

--- Frontend/admin-service-app/src/pages/Landing/Landing.jsx
+++ Frontend/admin-service-app/src/pages/Landing/Landing.jsx
@@ -4,12 +4,52 @@ import Input from '../../components/Input/Input';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { loginUser } from '../../query/auth/auth';
+import { useQuery, useMutation, useQueryClient} from "@tanstack/react-query";
+import { queryClient } from '../../App';
+
 const Landing =()=>{
-    const navigate = useNavigate();
-    const login =()=>{
-        navigate('/admin');
-    }
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const [error, setError]= useState(false);
+    const navigate = useNavigate();
+
+    const navigateToRoute = async ()=>{
+            const currentUser = await queryClient.getQueryData(["CurrentUser"]);
+            localStorage.setItem('token', currentUser.data.token);
+            if(currentUser.data.user.usertype === 'Seller'){
+                navigate('/service/followers');
+            }
+            else {
+                navigate('/admin/services');
+            }
+    }
+    const {mutate, isLoading} = useMutation(loginUser, {
+        onSuccess: (data) =>{
+            queryClient.setQueryData(["CurrentUser"], {...data})
+            navigateToRoute()
+        },
+        onError: (e) => {
+            setError(true)
+        },
+       onSettled: () => {
+         queryClient.invalidateQueries('create');
+       }
+     })
+
+    //function to submit login form
+    const submit = async (e)=>{
+        console.log('submit')
+        e.preventDefault();
+        const payload = {
+            email, 
+            password
+        }
+         mutate(payload);
+
+    }
+
+    // handeling errors
     let errorfield;
     if(error){
          errorfield = <p className='error '>Invalid Credentials</p>
@@ -22,19 +62,21 @@ const Landing =()=>{
                 <div className="logo-container">
                     <img src={logo} className="logo-image"/>
                 </div>
-                <form className='login-form flex column' onSubmit={login}>
+                <form className='login-form flex column' onSubmit={submit}>
                     <Input
                     type={'email'}
                     placeholder={"Enter your email"}
                     name={"landing-input"}
-                    label={"Email"}/>
+                    label={"Email"}
+                    onChange={(e)=>{setEmail(e.target.value)}}/>
                     <Input
                     type={'password'}
                     placeholder={"Enter your password"}
                     name={"landing-input"}
-                    label={"Password"}/>  
+                    label={"Password"}
+                    onChange={(e)=>setPassword(e.target.value)}/>  
                     {errorfield}
-                    <input type={"submit"} value="LOGIN" className="btn-login" onClick={()=>{setError(true)}}/>
+                    <input type={"submit"} value="LOGIN" className="btn-login"/>
 
                 </form>
             </div>
