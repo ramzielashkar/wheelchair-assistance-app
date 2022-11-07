c4b877d423b9bddd7e6a5f59f0cd63bcab3b1346
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-07 15:06:08 +0200

    Linking: Fetching banned services

--- Frontend/admin-service-app/src/pages/BannedServices/BannedServices.jsx
+++ Frontend/admin-service-app/src/pages/BannedServices/BannedServices.jsx
@@ -1,26 +1,29 @@
 import ServiceCard from "../../components/ServiceCard/ServiceCard";
 import './style.css';
+import { useBannedServices } from "../../query/ServiceProviders/useServiceProviders";
+import EmptyState from '../../components/EmptyState/EmptyState'
+import { CircularProgress } from "@mui/material";
 const BannedServices = () =>{
+    const { data: bannedServices, isLoading: isLoadingServices, isFetching: isFetchingServices } = useBannedServices();
+    if(isLoadingServices || isFetchingServices){
+        return(
+            <CircularProgress/>
+        );
+    }
+    if(bannedServices.service_providers.length==0){
+        return(
+            <EmptyState content={'No Banned Services'}/>
+        );
+    }
     return(
         <section className=" services-container">
-        <ServiceCard
-        name={"Hospital"}
-        path={"banned"}
-        content={"service"}/>
-        <ServiceCard
-        name={"Hospital"}
-        path={"banned"}
-        content={"service"}/>
-        <ServiceCard
-        name={"Hospital"}
-        path={"banned"}
-        content={"service"}/>
-        <ServiceCard
-        name={"Hospital"}
-        path={"banned"}
-        content={"service"}/>
 
+        <ServiceCard
+        name={"Hospital"}
+        path={"banned"}
+        content={"service"}/>
     </section>    
     );
+    
 }
 export default BannedServices;
