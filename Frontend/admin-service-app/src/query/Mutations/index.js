bc8f41931e0f789e0e30b14a2e4b467a2360ede3
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-07 18:43:27 +0200

    Linking: Creating ban/unban clients hook

--- Frontend/admin-service-app/src/query/Mutations/index.js
+++ Frontend/admin-service-app/src/query/Mutations/index.js
@@ -1,7 +1,7 @@
 import { axiosInstance } from "../axios/axios"
 import { ALL_ACTIVE_SERVICES_KEY, ALL_BANNED_SERVICES_KEY } from "../ServiceProviders/useServiceProviders"
+import { ALL_ACTIVE_CLIENTS_KEY, ALL_BANNED_CLIENTS_KEY } from "../AdminClients/useClients"
 export default (queryClient)=>{
-
 //function to ban/unban service provider
  queryClient.setMutationDefaults(["TOGGLE_SERVICE"],{
     mutationFn: (id) => 
@@ -13,6 +13,22 @@ export default (queryClient)=>{
             })
             queryClient.invalidateQueries({
                 queryKey: ALL_BANNED_SERVICES_KEY
+            })
+        }
+})
+
+
+//function to ban/unban service provider
+queryClient.setMutationDefaults(["TOGGLE_CLIENT"],{
+    mutationFn: (id) => 
+        axiosInstance.put(`admin/banClient/${id}`).then((res) => res.data),
+        onSuccess: (data) => {        
+            // Fetch all Services again 
+            queryClient.invalidateQueries({
+                queryKey: ALL_ACTIVE_CLIENTS_KEY
+            })
+            queryClient.invalidateQueries({
+                queryKey: ALL_BANNED_CLIENTS_KEY
             })
         }
 })
