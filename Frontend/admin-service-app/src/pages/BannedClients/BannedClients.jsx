a54fdc3bc746d9a79aeaaf7a4a0a2e949fca623f
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-07 15:56:26 +0200

    Linking: Fetching banned clients

--- Frontend/admin-service-app/src/pages/BannedClients/BannedClients.jsx
+++ Frontend/admin-service-app/src/pages/BannedClients/BannedClients.jsx
@@ -1,21 +1,38 @@
 import ServiceCard from '../../components/ServiceCard/ServiceCard';
 import './style.css';
+import {CircularProgress} from "@mui/material";
+import { useBannedClients } from '../../query/AdminClients/useClients';
+import EmptyState from '../../components/EmptyState/EmptyState';
 const BannedClients = ()=>{
+
+    //fetching banned clients
+    const { data: bannedClients, isLoading: isLoadingClients,  isFetching: isFetchingClients  } = useBannedClients();
+
+    // if clients data still fetching
+    if(isLoadingClients || isFetchingClients){
+        return(
+            <CircularProgress/>
+        );
+    }
+    // if no clients available
+    if(bannedClients.clients.length==0){
+        return(
+            <EmptyState content={'No Banned Clients'}/>
+        );
+    }
+
     return(
         <section className="clients-container">
-            <ServiceCard
-            name={"Hospital"}
-            path={"banned"}
-            />
-            <ServiceCard
-            name={"Hospital"}
-            path={"banned"}/>
-            <ServiceCard
-            name={"Hospital"}
-            path={"banned"}/>
-            <ServiceCard
-            name={"Hospital"}
-            path={"banned"}/>
+             {bannedClients?.clients?.map((client)=>{
+                return(
+                <ServiceCard
+                name={client.name}
+                path={"banned"}
+                id={client._id}
+                photo={client.profile_picture}
+                onClick={(id = client._id)=>{console.log(id)}}/>
+                )
+            })}
 
         </section>    
     );
