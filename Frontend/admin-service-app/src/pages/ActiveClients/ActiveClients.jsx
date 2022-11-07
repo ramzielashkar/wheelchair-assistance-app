6f9ad55530735d78fc75e1f799bc444c89c2c74a
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-07 15:56:16 +0200

    Linking: Fetching active clients

--- Frontend/admin-service-app/src/pages/ActiveClients/ActiveClients.jsx
+++ Frontend/admin-service-app/src/pages/ActiveClients/ActiveClients.jsx
@@ -1,25 +1,37 @@
 import ServiceCard from '../../components/ServiceCard/ServiceCard';
 import './style.css';
+import { useActiveClients } from '../../query/AdminClients/useClients';
+import {CircularProgress} from "@mui/material";
+import EmptyState from '../../components/EmptyState/EmptyState';
+
 const ActiveClients = ()=>{
+    //fetching active clients
+    const { data: activeClients, isLoading: isLoadingClients,  isFetching: isFetchingClients  } = useActiveClients();
+
+    //if clients data isn't fetched yet
+    if(isLoadingClients || isFetchingClients){
+        return(
+            <CircularProgress/>
+        );
+    }
+    //if no clients available
+    if(activeClients.clients.length==0){
+        return(
+            <EmptyState content={'No Active Clients'}/>
+        );
+    }
     return(
         <section className=" clients-container">
-            <ServiceCard
-            name={"Hospital"}
-            path={"active"}
-            />
-            <ServiceCard
-            name={"Hospital"}
-            path={"active"}/>
-            <ServiceCard
-            name={"Hospital"}
-            path={"active"}/>
-            <ServiceCard
-            name={"Hospital"}
-            path={"active"}/>
-            <ServiceCard
-            name={"Hospital"}
-            path={"active"}/>
-
+            {activeClients?.clients?.map((client)=>{
+                return(
+                <ServiceCard
+                name={client.name}
+                path={"active"}
+                id={client._id}
+                photo={client.profile_picture}
+                onClick={(id = client._id)=>{console.log(id)}}/>
+                )
+            })}
         </section>    
     );
 }
