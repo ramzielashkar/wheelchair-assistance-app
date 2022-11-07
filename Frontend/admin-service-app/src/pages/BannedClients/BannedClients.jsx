ef5063c9a096944cd670d17f5e27a5d2e25e182b
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-07 18:37:08 +0200

    Linking: Including ban client hook

--- Frontend/admin-service-app/src/pages/BannedClients/BannedClients.jsx
+++ Frontend/admin-service-app/src/pages/BannedClients/BannedClients.jsx
@@ -3,7 +3,21 @@ import './style.css';
 import {CircularProgress} from "@mui/material";
 import { useBannedClients } from '../../query/AdminClients/useClients';
 import EmptyState from '../../components/EmptyState/EmptyState';
+import { store } from '../../Redux/store';
+import { useNavigate } from 'react-router-dom';
+import { deleteUser } from '../../Redux/slices/userSlice';
+import { AuthVerify } from '../../authorization/jwtVerify';
+
 const BannedClients = ()=>{
+
+    const navigate = useNavigate();
+    //checking jwt token
+    const state = AuthVerify();
+    if(!state){
+        store.dispatch(deleteUser())
+        localStorage.setItem('token', '');
+        navigate('/')
+    }
 
     //fetching banned clients
     const { data: bannedClients, isLoading: isLoadingClients,  isFetching: isFetchingClients  } = useBannedClients();
