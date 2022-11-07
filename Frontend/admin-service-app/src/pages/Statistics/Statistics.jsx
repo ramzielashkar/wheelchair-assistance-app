257397310cf8614cb6148c433a97b6bb825bffbf
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-07 18:45:31 +0200

    Frontend: Updating statistics page

--- Frontend/admin-service-app/src/pages/Statistics/Statistics.jsx
+++ Frontend/admin-service-app/src/pages/Statistics/Statistics.jsx
@@ -1,16 +1,25 @@
 import './style.css';
 import {CanvasJSChart} from 'canvasjs-react-charts'
+import { useStats } from '../../query/AdminStats/useStats';
+import { CircularProgress } from '@mui/material';
+
 
 const Statistics = ()=>{
 
+    // fetching statistics
+    const { data: usersCount, isLoading: isLoadingCount, isFetching: isFetchingCount } = useStats();
+
+    //if data is still fetching
+    if(isLoadingCount || isFetchingCount){
+        return <CircularProgress/>
+    }
     const options = {
-   
         data: [
         {
             type: "column",
             dataPoints: [
-                { label: "Clients",  y: 10  },
-                { label: "Service Providers", y: 15  },
+                { label: "Clients",  y: usersCount.clients  },
+                { label: "Service Providers", y: usersCount.service_providers  },
             ]
         }
         ]
