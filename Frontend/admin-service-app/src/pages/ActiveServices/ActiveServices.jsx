1554f8fc506188c31779a20fd8df85ec245a379b
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-07 15:05:55 +0200

    Linking: Fetching active services

--- Frontend/admin-service-app/src/pages/ActiveServices/ActiveServices.jsx
+++ Frontend/admin-service-app/src/pages/ActiveServices/ActiveServices.jsx
@@ -1,27 +1,37 @@
 import ServiceCard from '../../components/ServiceCard/ServiceCard';
 import './style.css';
+import { useActiveServices } from '../../query/ServiceProviders/useServiceProviders';
+import {CircularProgress} from "@mui/material";
+import { useMutation } from '@tanstack/react-query';
+
 const ActiveServices = ()=>{
+    const { data: activeServices, isLoading: isLoadingServices, isFetching: isFetchingServices  } = useActiveServices();
+    
+    const { mutate } = useMutation(["TOGGLE_SERVICE"])
+
+    if(isLoadingServices || isFetchingServices){
+        return(
+            <CircularProgress/>
+        );
+    }
     return(
-        <section className=" services-container">
-            <ServiceCard
-            name={"Hospital"}
-            path={"active"}
-            content={"service"}/>
-            <ServiceCard
-            name={"Hospital"}
-            path={"active"}
-            content={"service"}/>
-            <ServiceCard
-            name={"Hospital"}
-            path={"active"}
-            content={"service"}/>
-            <ServiceCard
-            name={"Hospital"}
-            path={"active"}
-            content={"service"}/>
+        <section className="services-container">
+            {activeServices?.service_providers?.map((service)=>{
+                return(
+                <ServiceCard
+                name={service.name}
+                location={service.location}
+                path={"active"}
+                content={"service"}
+                id={service._id}
+                onClick={(id = service._id)=>{mutate({id})}}/>
+                )
+            })}
+            
 
         </section>    
     );
 }
+
 
 export default ActiveServices;
