7b685908e78a439f628bec6748dba3933b649077
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-07 21:18:25 +0200

    Linking: Creating add service hook

--- Frontend/admin-service-app/src/query/Mutations/index.js
+++ Frontend/admin-service-app/src/query/Mutations/index.js
@@ -1,6 +1,8 @@
 import { axiosInstance } from "../axios/axios"
 import { ALL_ACTIVE_SERVICES_KEY, ALL_BANNED_SERVICES_KEY } from "../ServiceProviders/useServiceProviders"
 import { ALL_ACTIVE_CLIENTS_KEY, ALL_BANNED_CLIENTS_KEY } from "../AdminClients/useClients"
+import { useQuery} from "@tanstack/react-query";
+
 export default (queryClient)=>{
 //function to ban/unban service provider
  queryClient.setMutationDefaults(["TOGGLE_SERVICE"],{
@@ -32,4 +34,26 @@ queryClient.setMutationDefaults(["TOGGLE_CLIENT"],{
             })
         }
 })
+
+
+
+//function to add service provider
+queryClient.setMutationDefaults(["ADD_SERVICE"],{
+    mutationFn: (payload) => 
+        axiosInstance.post(`admin/addUser`, {...payload}).then((res) => res.data),
+        onSuccess: (data) => {        
+            // Fetch all Services again 
+            queryClient.invalidateQueries({
+                queryKey: ALL_ACTIVE_SERVICES_KEY
+            })
+            queryClient.invalidateQueries({
+                queryKey: ALL_BANNED_SERVICES_KEY
+            })
+        },
+        onError:(err)=>{
+        }
+})
+
+
 }
+
