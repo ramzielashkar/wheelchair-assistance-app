d3551350a24eb853f6cf768262d429cd2f49a599
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-07 15:07:52 +0200

    Linking: Updating redux user slice

--- Frontend/admin-service-app/src/Redux/slices/userSlice.js
+++ Frontend/admin-service-app/src/Redux/slices/userSlice.js
@@ -1,18 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-export const user = {
-    id:0,
-    profile_picture:'',
-    name:'',
-    active: false,
-    followers:[],
-    picture:[],
-    email:'',
-    description:'',
-    location:'',
-    geo_location :{},
-    usertype:"",
-}
 const initialState = {
     user:null,
 }
