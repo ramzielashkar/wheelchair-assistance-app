b18a6473f5f55fed5957ad4a267f48dc618d4b76
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-07 15:57:16 +0200

    Linking: Including empty state

--- Frontend/admin-service-app/src/pages/ActiveServices/ActiveServices.jsx
+++ Frontend/admin-service-app/src/pages/ActiveServices/ActiveServices.jsx
@@ -3,15 +3,24 @@ import './style.css';
 import { useActiveServices } from '../../query/ServiceProviders/useServiceProviders';
 import {CircularProgress} from "@mui/material";
 import { useMutation } from '@tanstack/react-query';
+import EmptyState from '../../components/EmptyState/EmptyState';
 
 const ActiveServices = ()=>{
+    //getting active service providers
     const { data: activeServices, isLoading: isLoadingServices, isFetching: isFetchingServices  } = useActiveServices();
     
     const { mutate } = useMutation(["TOGGLE_SERVICE"])
 
+    // if service providers aren't fetched yet
     if(isLoadingServices || isFetchingServices){
         return(
             <CircularProgress/>
+        );
+    }
+    //if no active service providers
+    if(activeServices.service_providers.length==0){
+        return(
+            <EmptyState content={'No Active Services'}/>
         );
     }
     return(
@@ -23,6 +32,7 @@ const ActiveServices = ()=>{
                 location={service.location}
                 path={"active"}
                 content={"service"}
+                photo={service.profile_picture}
                 id={service._id}
                 onClick={(id = service._id)=>{mutate({id})}}/>
                 )
