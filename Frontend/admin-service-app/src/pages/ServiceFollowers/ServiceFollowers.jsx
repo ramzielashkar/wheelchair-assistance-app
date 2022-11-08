effa57c8daf6995dfa893216e79f7a3664cc533e
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-08 13:45:11 +0200

    Linking: Including  get followers hook

--- Frontend/admin-service-app/src/pages/ServiceFollowers/ServiceFollowers.jsx
+++ Frontend/admin-service-app/src/pages/ServiceFollowers/ServiceFollowers.jsx
@@ -1,27 +1,46 @@
+import { CircularProgress } from "@mui/material";
+import { useState } from "react";
+import EmptyState from "../../components/EmptyState/EmptyState";
 import ServiceCard from "../../components/ServiceCard/ServiceCard";
+import { useFollowers } from "../../query/ServiceFollowers/useFollowers";
 const ServiceFollowers = ()=>{
+      //getting followers
+      const { data: followers, isLoading: isLoadingFollowers, isFetching: isFetchingFollowers, isFetched: fetched   } = useFollowers();
+       // if followers aren't fetched yet
+    if(isLoadingFollowers || isFetchingFollowers){
+        return(
+            <CircularProgress/>
+        );
+    }
+    //if no followers
+    if(fetched && followers.followers.followers.length==0){
+        return(
+        <section className="flex column clients-section">
+            <div className="clients-header flex">
+                <p className="clients-title">Followers</p>
+            </div>
+            <EmptyState content={'No Followers'}/>
+            </section>
+        );
+    }
     return(
         <section className="flex column clients-section">
             <div className="clients-header flex">
                 <p className="clients-title">Followers</p>
             </div>
+
             <div className="clients-container">
-            <ServiceCard
-            name={"Hospital"}
-            content="follower"
-            />
-            <ServiceCard
-            name={"Hospital"}
-            content="follower"
-            />
-            <ServiceCard
-            name={"Hospital"}
-            content="follower"
-            />
-            <ServiceCard
-            name={"Hospital"}
-            content="follower"
-            />
+            {followers?.followers?.followers?.map((follower)=>{
+                return(
+                <ServiceCard
+                name={follower.follower_id.name}
+                content={"follower"}
+                photo={follower.follower_id.profile_picture}
+                id={follower.follower_id._id}
+                />
+                )
+            })}
+            
             </div>
         </section>
     );
