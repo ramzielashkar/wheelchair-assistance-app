4cfa59b8d572d66dfccd60c3b9e2537ddbd9712c
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-08 14:10:18 +0200

    Linking: Creating update profile pic hook

--- Frontend/admin-service-app/src/query/Mutations/index.js
+++ Frontend/admin-service-app/src/query/Mutations/index.js
@@ -2,6 +2,8 @@ import { axiosInstance } from "../axios/axios"
 import { ALL_ACTIVE_SERVICES_KEY, ALL_BANNED_SERVICES_KEY } from "../ServiceProviders/useServiceProviders"
 import { ALL_ACTIVE_CLIENTS_KEY, ALL_BANNED_CLIENTS_KEY } from "../AdminClients/useClients"
 import { useQuery} from "@tanstack/react-query";
+import { store } from "../../Redux/store";
+import { updateUser } from "../../Redux/slices/userSlice";
 
 export default (queryClient)=>{
 //function to ban/unban service provider
@@ -54,6 +56,18 @@ queryClient.setMutationDefaults(["ADD_SERVICE"],{
         }
 })
 
+//function to change profile picture
+queryClient.setMutationDefaults(["UPDATE_PICTURE"],{
+    mutationFn: (payload) => 
+        axiosInstance.put(`service/profilepic`, {...payload}).then((res) => res.data),
+        onSuccess: (data) => {        
+           store.dispatch(updateUser({
+                user: data.user
+            }))
+        },
+        onError:(err)=>{
+        }
+})
 
 }
 
