e20f9ae01a84380fa4b769992c9d0626f2035178
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-09 12:11:38 +0200

    Frontend: Including redux provider

--- Frontend/client-app/App.js
+++ Frontend/client-app/App.js
@@ -1,9 +1,15 @@
 import StackSwitcher from './navigation/StackSwitcher';
-import TabsStack from './navigation/TabsStack';
-
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from './Redux/store';
+import { Provider } from 'react-redux';
 
 export default function App() {
   return (
+    <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <StackSwitcher/>
+    </PersistGate>
+    </Provider>
+
     );
 }
