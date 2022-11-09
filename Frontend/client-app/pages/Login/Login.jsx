019335baae3c2996b455a28a79a687ae39b9b9d4
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-09 15:14:25 +0200

    Linking: Including login hook

--- Frontend/client-app/pages/Login/Login.jsx
+++ Frontend/client-app/pages/Login/Login.jsx
@@ -2,7 +2,45 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 import styles from './styles';
 import Input from '../../components/Inputs/Inputs';
 import Buttons from '../../components/Button/Button';
+import { useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { store } from '../../Redux/store';
+import { updateUser } from '../../Redux/Slices/userSlice';
+import {  useMutation} from "@tanstack/react-query";
+import { loginUser } from '../../query/auth/auth';
+
 const Login = () =>{
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    //function to login
+    const {mutate, isLoading} = useMutation(loginUser, {
+        onSuccess: (data) =>{
+            AsyncStorage.setItem('token', data.data.token);
+            store.dispatch(updateUser({
+                user: data.data.user
+            }))
+        },
+        onError: (e) => {
+           setError('Invalid Credentials')
+        }
+     })
+    const handleSubmit = ()=>{
+        if(email==''){
+            setError('Email is required')
+        }
+        else if(password==''){
+            setError('Password is required')
+        }
+        else{
+        const payload = {
+            email,
+            password
+        }
+        mutate(payload)
+    }
+    }
     return(
         <View style={styles.root}>
             <View>
@@ -12,13 +50,21 @@ const Login = () =>{
                 <Input 
                 label={"Email"}
                 placeholder={"Enter your email:"}
-                type ={"email-address"}/>
+                type ={"email-address"}
+                onChange={(e)=>setEmail(e)}
+                onFocus={()=>setError('')}
+                />
                 <Input 
                 label={"Password"}
                 placeholder={"Enter your password:"}
-                type={"password"}/>
+                type={"password"}
+                onChange={(e)=>setPassword(e)}
+                onFocus={()=>setError('')}
+                />
+                {error && <Text style={styles.errorField}>{error}</Text>}
                 <Buttons
-                title={"LOGIN"}/>
+                title={"LOGIN"}
+                onClick={handleSubmit}/>
 
             </View>
         </View>
