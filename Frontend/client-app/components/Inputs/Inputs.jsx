25ab08f57259078f6c75b63e72cb5bac8b139040
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-09 14:22:06 +0200

    Frontend: Updating input component

--- Frontend/client-app/components/Inputs/Inputs.jsx
+++ Frontend/client-app/components/Inputs/Inputs.jsx
@@ -1,11 +1,11 @@
 import { Text, TextInput, View } from 'react-native';
 import styles from './styles';
-const Input = ({label, type, placeholder}) =>{
+const Input = ({label, type, placeholder, onChange, onFocus}) =>{
     if(type=='password'){
         return (
             <View style={styles.user_info}>
                 <Text style={styles.labels}>{label}</Text>
-                <TextInput  keyboardType = 'default'  placeholder={placeholder} style={styles.input} secureTextEntry='true'></TextInput>
+                <TextInput keyboardType = 'default'  placeholder={placeholder} style={styles.input} secureTextEntry onChangeText={onChange} onFocus={onFocus}></TextInput>
             </View>
         );
     }
@@ -13,14 +13,14 @@ const Input = ({label, type, placeholder}) =>{
     {
         return(
             <View style={styles.user_info}>
-                    <TextInput keyboardType={type}  placeholder={placeholder} style={styles.input}></TextInput>
+                    <TextInput keyboardType={type}  placeholder={placeholder} style={styles.input} onChangeText={onChange} onFocus={onFocus}></TextInput>
                 </View>
         );
     }else{
     return(
         <View style={styles.user_info}>
                 <Text style={styles.labels}>{label}</Text>
-                <TextInput keyboardType={type}  placeholder={placeholder} style={styles.input}></TextInput>
+                <TextInput keyboardType={type}  placeholder={placeholder} style={styles.input} onChangeText={onChange} onFocus={onFocus}></TextInput>
             </View>
     );
     }
