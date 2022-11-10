6ddf2931a95825baf6499ef7a7c0853f9775df69
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-10 14:37:58 +0200

    Frontend: Showing user location on map

--- Frontend/client-app/pages/Map/Map.jsx
+++ Frontend/client-app/pages/Map/Map.jsx
@@ -2,23 +2,26 @@ import { Dimensions, Text, View } from "react-native";
 import MapView,{PROVIDER_GOOGLE, Marker, LatLng} from 'react-native-maps';
 import styles from "./styles";
 import { GOOGLE_API_KEY } from "../../Credentials/credentials";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MapViewDirections from 'react-native-maps-directions';
 import Buttons from "../../components/Button/Button";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useSelector } from "react-redux";
 
 const Map = ()=>{
-    const [location, setLocation]= useState('');
+    const [location, setLocation]= useState(useSelector((state)=>state.user.location));
     const [showDirections, setShowDirections]=useState(false);
     const {width, height} = Dimensions.get("window");
-    const origin = {
-        latitude : 33.793791,
-        longitude: 35.401778
-    }
+            console.log("user: ", location)
+  /*  useEffect(()=>{
+        ( ()=>{
+            
+        })();
+  }, []);*/
     const dest = {
         latitude : 33.952141,
         longitude: 35.592972
     }
-
     const ASPECT_RATIO = width/height;
     const LATITUDE_DELTA = 0.02;
     const LONGITUDE_DELTA =LATITUDE_DELTA*ASPECT_RATIO;
@@ -39,9 +42,9 @@ const Map = ()=>{
                     latitude : 33.893791,
                     longitude: 35.501778
                 }}/>
-                <Marker coordinate={origin}/>
+                <Marker coordinate={location}/>
                 <MapViewDirections 
-                origin={origin}
+                origin={location}
                 destination={dest}
                 apikey={GOOGLE_API_KEY}/>
                 </MapView>
