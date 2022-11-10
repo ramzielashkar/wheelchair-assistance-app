9f978624cf56fb9c556a15c507e2721952e6868f
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-10 17:16:14 +0200

    Frontend: Updating axios instance

--- Frontend/client-app/query/axios/index.js
+++ Frontend/client-app/query/axios/index.js
@@ -1,16 +1,16 @@
 import axios from "axios";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-const token = AsyncStorage.getItem('token');
-export const baseUrl = "http://192.168.1.66:8000"
-export const axiosInstance = axios.create(
-    {
-        baseURL: "http://192.168.1.66:8000",
-        headers:{
-            'Authorization': `Bearer ${token}`,
-            "Access-Control-Allow-Origin": "*",
-            'Access-Control-Allow-Headers': "*",
-            'Access-Control-Allow-Methods': "GET, POST, PUT, DELETE",
-            'Access-Control-Allow-Credentials': true
+import { baseUrl } from "../../Credentials/credentials";
+export default(token)=>{
+    return axios.create(
+        {
+            baseURL: baseUrl,
+            headers:{
+                'Authorization':  `Bearer ${token}`,
+                "Access-Control-Allow-Origin": "*",
+                'Access-Control-Allow-Headers': "*",
+                'Access-Control-Allow-Methods': "GET, POST, PUT, DELETE",
+                'Access-Control-Allow-Credentials': true
+            }
         }
-    }
-)
+    )
+}
