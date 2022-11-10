5de4e4344e27cf57851a9770dd021aaec2fe80a6
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-10 20:40:40 +0200

    Linking: Including fetched service providers

--- Frontend/client-app/pages/Home/Home.jsx
+++ Frontend/client-app/pages/Home/Home.jsx
@@ -9,23 +9,16 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { store } from "../../Redux/store";
 import { updateLocation } from "../../Redux/Slices/userSlice";
 import { useSelector } from "react-redux";
-
-const data = [
-    {
-        name:"Restaurant",
-        location:"Beirut, Lebanon"
-},
-{
-    name:"Restaurant",
-    location:"Beirut, Lebanon"
-},
-
-]
+import { useHospitals, useRestaurants, useVendors } from "../../query/ServiceProviders/useServiceProviders";
+import { getLatitude } from "../../query/getLatitude";
+import { getLongitude } from "../../query/getLongitude";
 
 const Home = ({navigation}) =>{
-    const [location, setLocation] = useState(null);
-    const [errorMsg, setErrorMsg] = useState(null);
+    const [location, setLocation] = useState('');
+    const [errorMsg, setErrorMsg] = useState('');
     const [userLocation, setUserLocation]= useState({});
+    
+    
     useEffect(()=>{
         (async ()=>{
         let { status } = await Location.requestForegroundPermissionsAsync();
@@ -46,67 +39,89 @@ const Home = ({navigation}) =>{
                 longitude: location.coords.longitude
               }
         }))
+
     })();
+    
 
   }, []);
  
+ 
     //function to navigate to service page
-    const navigateToService=(name)=>{
-        navigation.navigate('Service', {name:name});
+    const navigateToService=(item)=>{
+        navigation.navigate('Service', {service:item, name:item.name});
     }
     const navigateToServices = (type)=>{
         navigation.navigate('Services', {type:type});
 
     }
-    const count =1;
+    //getting user geo location form redux store
+    const latitude = useSelector((state)=>state.user.geo_location.latitude? state.user.geo_location.latitude : ' ')
+    const longitude = useSelector((state)=>state.user.geo_location.longitude? state.user.geo_location.longitude: " " )
+
 let screenWidth = Dimensions.get('window').width;
 let flatListStyle;
- console.log(screenWidth);
- if(screenWidth<450){
-    flatListStyle={justifyContent: "space-between"}
- }else if(screenWidth>450){
-    flatListStyle={gap:20}
- }
- let columns=2;
- if(screenWidth > 450){
-    columns=3;
- }
-    if(count==0){
+let columns=2;
+
+
+// getting service providers
+ const { data: hospitals, isLoading: isLoadingHospitals,  isFetching: isFetchingHospitals  } = useHospitals(latitude, longitude);
+ const { data: restaurants, isLoading: isLoadingRestaurants,  isFetching: isFetchingRestaurants  } = useRestaurants(latitude, longitude );
+ const { data: vendors, isLoading: isLoadingVendors,  isFetching: isFetchingVendors  } = useVendors(latitude, longitude);
+ 
+ // if no service providers
+    if(hospitals && vendors && restaurants && hospitals.user.length==0 && vendors.user.length==0 && restaurants.user.length==0){
         return(
             <EmptyState
-                content={'hospitals'}
+                content={'Services'}
                 icon={'map-marker'}
             />
         );
     }
+    // managing different screen sizes
+    if(screenWidth<450){
+        flatListStyle={justifyContent: "space-between"}
+        if(hospitals?.user.length > 2){
+            hospitals.user = hospitals.user.slice(0, 2)
+        }
+        if(restaurants?.user.length > 2){
+            restaurants.user = restaurants.user.slice(0, 2)
+        }
+        if(vendors?.user.length > 2){
+            vendors.user = vendors.user.slice(0, 2)
+        }
+     }else if(screenWidth>450){
+        flatListStyle={gap:20}
+        columns=3;
+     }
+   
     return(
         <ScrollView style={styles.root}>
-            <SafeAreaView style={styles.serviceContainer}>
+           {hospitals?.user.length!=0 &&<SafeAreaView style={styles.serviceContainer}>
                 <View style={styles.header}>
                     <Text style={styles.title}>Hospitals</Text>
                     <Text style={styles.more} onPress={() => navigateToServices('Hospitals')}>View more</Text>
                 </View>
                 <FlatList 
                     columnWrapperStyle={flatListStyle}
-                    data={data}
+                    data={hospitals?.user}
                     renderItem={({item})=>(
                         <ServiceCard
-                            press={()=>{navigateToService(item.name)}}
+                            press={()=>{navigateToService(item)}}
                             data={item}/>
                     )}
                     numColumns={columns}
                     >
                 </FlatList>
-            </SafeAreaView>
+            </SafeAreaView>}
 
-            <SafeAreaView style={styles.serviceContainer}>
+           { restaurants?.user.length!=0 &&<SafeAreaView style={styles.serviceContainer}>
                 <View style={styles.header}>
                     <Text style={styles.title}>Restaurants</Text>
                     <Text style={styles.more} onPress={() => navigateToServices('Restaurants')}>View more</Text>
                 </View>
                 <FlatList 
                     columnWrapperStyle={flatListStyle}
-                    data={data}
+                    data={restaurants?.user}
                     renderItem={({item})=>(
                         <ServiceCard
                         press={()=>{navigateToService(item.name)}}
@@ -115,15 +130,15 @@ let flatListStyle;
                     numColumns={columns}
                     >
                 </FlatList>
-            </SafeAreaView>
-            <SafeAreaView style={styles.serviceContainer}>
+            </SafeAreaView>}
+            { vendors?.user.length!=0  && <SafeAreaView style={styles.serviceContainer}>
                 <View style={styles.header}>
                     <Text style={styles.title}>Vendors</Text>
                     <Text style={styles.more} onPress={() => navigateToServices('Vendors')}>View more</Text>
                 </View>
                 <FlatList 
                     columnWrapperStyle={flatListStyle}
-                    data={data}
+                    data={vendors?.user}
                     renderItem={({item})=>(
                         <ServiceCard
                         press={()=>{navigateToService(item.name)}}
@@ -132,7 +147,7 @@ let flatListStyle;
                     numColumns={columns}
                     >
                 </FlatList>
-            </SafeAreaView>
+            </SafeAreaView>}
         </ScrollView>
     );
 }
