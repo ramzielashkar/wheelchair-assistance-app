8d335071ba19432c051025ceb24134c412e98a31
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-12 17:02:30 +0200

    Linking: Including search hook

--- Frontend/client-app/pages/Search/Search.jsx
+++ Frontend/client-app/pages/Search/Search.jsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from "react";
 import { Text, View,Dimensions, ScrollView, FlatList } from "react-native";
 import Input from "../../components/Inputs/Inputs";
 import ServiceCard from "../../components/ServiceCard/ServiceCard";
+import { useResults } from "../../query/Search/useSearchResults";
 import styles from "./styles";
 const data = [
     {
@@ -17,9 +19,18 @@ const data = [
 },
 ]
 const Search = ({navigation}) =>{
+    const [searchResult, setSearchResult] = useState('');
+    const [searchKey, setSearchKey] = useState('');
+    const [search, setSearch] = useState(false);
+
     const navigateToService=(name)=>{
         navigation.navigate('Service', {name:name});
     }
+
+    if(search){
+        const {data:results} = useResults(searchKey)
+    }
+
     let screenWidth = Dimensions.get('window').width
     let flatListStyle;
  console.log(screenWidth);
@@ -38,12 +49,17 @@ const Search = ({navigation}) =>{
  if(screenWidth > 600){
     columns=3;
  }
- 
+ if(searchKey!=''){
+ }
+    //console.log(results)
     return(
         <ScrollView style={styles.root}>
             <Input 
             placeholder={"Search here..."}
-            type={"search"}/>
+            type={"search"}
+            onChange={(e)=>{setSearchKey(e)
+            setSearch(true)}}
+            />
 
             <View style={styles.serviceContainer}>
             <FlatList 
