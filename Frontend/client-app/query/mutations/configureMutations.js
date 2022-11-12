f7857b9d4235b1e52f1961284ecd9da622e3e163
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-12 18:50:21 +0200

    Linking: Updating update profile hook

--- Frontend/client-app/query/mutations/configureMutations.js
+++ Frontend/client-app/query/mutations/configureMutations.js
@@ -25,9 +25,6 @@ queryClient.setMutationDefaults(["UPDATE_PROFILE"],{
     mutationFn: (payload) => 
         axiosInstance(getToken()).put(`client/editprofile`, {...payload}).then((res) => res.data),
         onSuccess: (data) => { 
-          store.dispatch(updateUser({
-                user: data.user
-            }))
         },
         onError:(err)=>{
            console.log(err.response)
