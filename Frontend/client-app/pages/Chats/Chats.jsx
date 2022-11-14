cb5aabbd4129f193a91100f47272058f62065aa5
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-14 20:39:29 +0200

    Linking: Getting user related chats from firebase

--- Frontend/client-app/pages/Chats/Chats.jsx
+++ Frontend/client-app/pages/Chats/Chats.jsx
@@ -1,50 +1,88 @@
 import { Text, ScrollView, FlatList, View } from "react-native";
 import ChatCard from "../../components/ChatCard/ChatCard";
 import styles from "./styles";
-const data = [
-    {
-        img:  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnJyBH7aR8qVizMo5qDuRnGJu8dn_uEmF3EA&usqp=CAU",
-        name:"Ramzi",
-        content:"Hello, how are you",
-        date:"Oct,10, 12:00PM"
+import { firebaseDB } from '../../configurations/firebaseConfiguration';
+import { getDatabase, onValue, push, ref, set, get, update } from "firebase/database";
+import { useSelector } from "react-redux";
+import { useService } from "../../query/ServiceProviders/useServiceProviders";
+import { getToken } from "../../query/getToken";
+import axiosInstance from '../../query/axios/index'
+import { baseUrl } from "../../Credentials/credentials";
+import { useEffect, useState } from "react";
 
-    },
-    {
-        img:  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnJyBH7aR8qVizMo5qDuRnGJu8dn_uEmF3EA&usqp=CAU",
-        name:"Marwan",
-        content:"Hello, how are youfdfdf dfdfdf dfdfdf dfdfdf dfdfdf ",
-        date:"Oct,10, 12:00PM"
-
-    },
-    {
-        img:  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnJyBH7aR8qVizMo5qDuRnGJu8dn_uEmF3EA&usqp=CAU",
-        name:"Karam",
-        content:"Hello, how are you",
-        date:"Oct,10, 12:00PM"
-
-    },
-    {
-        img:  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnJyBH7aR8qVizMo5qDuRnGJu8dn_uEmF3EA&usqp=CAU",
-        name:"Razan",
-        content:"Hello, how are you",
-        date:"Oct,10, 12:00PM"
-
-    }
-]
 const Chats = ({navigation})=>{
-    return(
-        <View style={styles.root}>
-            <View style={styles.chatsContainer}>
-                <FlatList
-                data={data}
-                renderItem={({item})=>(
-                    <ChatCard
-                    onClick={()=>{navigation.navigate('Chat', {name:item.name})}}
-                    data={item}/>
-                )}>
-                </FlatList>
+    const loggedInUser = useSelector((state)=>state.user._id)
+    const [data, setData] = useState('')
+    useEffect(()=>{
+        //fetching previous chats form firebase
+        const conversations = [];
+        let Data=[]
+        get(ref(firebaseDB,'chats/')).then((snapshot)=>{
+            if(snapshot.exists()){
+                const data = snapshot.val();
+                for (const [key, value] of Object.entries(data)) {
+                    //getting chats related to user
+                    if(value.firstUserId == loggedInUser || value.secondUserId == loggedInUser){
+                        conversations.push(value)
+    
+                    }
+                }
+                conversations.map((conversation)=>{
+                    if(conversation.firstUserId == loggedInUser){
+                        //getting seller info from database
+                        axiosInstance(getToken()).get(`/client/service/${conversation.secondUserId}`).then(
+                            (res)=>{
+                                Data.push({
+                                    _id: res.data.seller._id,
+                                    img: `${baseUrl}/public/${res.data.seller.profile_picture}`,
+                                    content: conversation.latestMessage.text,
+                                    date:conversation.latestMessage.createdAt,
+                                    name: res.data.seller.name
+                                })
+                                setData(Data)
+
+                            }
+                            
+                            )
+    
+                    }else if(conversation.secondUserId == loggedInUser){
+                     //getting seller info from database
+                        axiosInstance(getToken()).get(`/client/service/${conversation.firstUserId}`).then(
+                            (res)=>{
+                                Data.push({
+                                    _id: res.data.seller._id,
+                                    img: `${baseUrl}/public/${res.data.seller.profile_picture}`,
+                                    content: conversation.latestMessage.text,
+                                    date:conversation.latestMessage.createdAt,
+                                    name: res.data.seller.name
+                                })
+                                setData(Data)
+
+                            }
+                            
+                            )
+                    }
+                })
+                   
+            }
+           })
+    }, [])
+
+
+        return(
+            <View style={styles.root}>
+                <View style={styles.chatsContainer}>
+                    <FlatList
+                    data={data}
+                    renderItem={({item})=>(
+                        <ChatCard
+                        onClick={()=>{navigation.navigate('Chat', {service: item ,name:item.name})}}
+                        data={item}/>
+                    )}>
+                    </FlatList>
+                </View>
             </View>
-        </View>
-        );
-}
+            );
+                }
+
 export default Chats;
