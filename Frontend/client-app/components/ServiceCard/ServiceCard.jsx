663c71a125838750c2c29f7cf5545bc7dc95674b
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-16 20:19:02 +0200

    Frontend: Updating service card component

--- Frontend/client-app/components/ServiceCard/ServiceCard.jsx
+++ Frontend/client-app/components/ServiceCard/ServiceCard.jsx
@@ -9,14 +9,14 @@ const ServiceCard = ({data, press}) => {
     return (
         <View style={styles.container}>
             <View>
-                <Image style={styles.image} source={{uri:`${baseUrl}/public/${data.profile_picture}`}}></Image>
+                <Image style={styles.image} source={{uri:`${baseUrl}/public/${data?.profile_picture}`}}></Image>
             </View>
             <View style={styles.unfollow}>
-                    <Text numberOfLines={1} style={styles.name}>{data.name}</Text>
+                    <Text numberOfLines={1} style={styles.name}>{data?.name}</Text>
                 </View>
             <View style={styles.location}>
                 <Ionicons name="location-outline" size={15} color="black" />
-                <Text numberOfLines={1} style={styles.location}>{data.location}</Text>
+                <Text numberOfLines={1} style={styles.location}>{data?.location}</Text>
                 <View style={styles.more}>
                 <TouchableOpacity style={styles.more_info} onPress={press}>
                     <AntDesign name="caretright" size={11} color="white" />
