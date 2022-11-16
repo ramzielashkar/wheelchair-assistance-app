311951968474b7a53e108004273a46544391b188
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-16 20:21:13 +0200

    Linking: Deleting unused imports

--- Frontend/client-app/App.js
+++ Frontend/client-app/App.js
@@ -2,12 +2,22 @@ import StackSwitcher from './navigation/StackSwitcher';
 import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from './Redux/store';
 import { Provider } from 'react-redux';
+import {QueryClientProvider} from "@tanstack/react-query";
+import {QueryClient} from "@tanstack/react-query";
+export const queryClient = new QueryClient();
+import configureMutations from './query/mutations/configureMutations';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+configureMutations(queryClient)
 
 export default function App() {
   return (
     <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
+    <QueryClientProvider client={queryClient}>
     <StackSwitcher/>
+    </QueryClientProvider>
+
     </PersistGate>
     </Provider>
 
