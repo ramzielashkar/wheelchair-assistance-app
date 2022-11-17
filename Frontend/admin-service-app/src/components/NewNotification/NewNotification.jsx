cdae066abfe323b77d62514c21ed72227e356509
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-11-17 15:32:01 +0200

    Linking: Updating new notification popup

--- Frontend/admin-service-app/src/components/NewNotification/NewNotification.jsx
+++ Frontend/admin-service-app/src/components/NewNotification/NewNotification.jsx
@@ -1,25 +1,35 @@
 import './style.css';
-import Button from '../Button/Button';
 import {MdOutlineClose} from 'react-icons/md';
+import { useMutation } from '@tanstack/react-query';
+import { useState } from 'react';
 
 const NewNotification = ({isOpen, onClose})=>{
+        const [notification, setNotification] = useState('')
+        const {mutate} = useMutation(["NOTIFY"]);
+
+        //function to send notification
+        const sendNotification = (e)=>{
+            e.preventDefault();
+            const payload ={
+                notification
+            }
+            mutate(payload)
+            onClose()
+        }
         if(isOpen){
             return(
                 <section className='flex column new-notification-section'>
                     
-                    <form className='flex column new-notification-form'>
+                    <form className='flex column new-notification-form' onSubmit={sendNotification}>
                     <div className='new-notification-header flex'>
                         <p>New Notification</p>
                         <div className='flex close-popup' onClick={onClose}>
                         <MdOutlineClose size={30}/>
                         </div>
                     </div>
-                        <textarea className="new-notification" cols={60} rows={20} placeholder="New Notification..."></textarea>
+                        <textarea className="new-notification" required cols={60} rows={20} placeholder="New Notification..." onChange={(e)=>setNotification(e.target.value)}></textarea>
                         <div className="flex btns">
-                        <Button
-                        text={"Send Notification"}
-                        onClick={onClose}
-                        />
+                        <input type={'submit'} className="btn" value={"Send Notification"}></input>
                         </div>
                         
                     </form>
